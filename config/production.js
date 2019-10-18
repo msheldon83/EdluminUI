60f8027839bee994a07277b3fde86cfb155645c6
Frank Dzedzy <fdzedzy@edustaff.org>
2019-10-18 12:12:50 -0400

    correct redirect url and update pipeline file

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   minify: true,
   auth0_domain: "hcmdev.auth0.com",
   auth0_client: "eLiUtXy2kn7cRvjpl3o7oQGCTlbbxeNS",
-  auth0_redirect_url: "http://localhost:3000/",
+  auth0_redirect_url: "https://dev.edlumin.com/",
   auth0_api_audience: "https://hcmdev/api",
   auth0_scope: "openid profile email",
   apiUrl: "https://devapi.edlumin.com/graphql"
