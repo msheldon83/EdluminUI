e80448d3f72b9039b0ecebae13e9881aef94f2fc
David Nawn <dtnawn@gmail.com>
2019-10-21 14:55:40 -0400

    Add storybook examples for Position Types list

---
+++ modules/ui/pages/position-type/index.stories.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { PositionTypePage } from "./index";
+import { mockProvider } from "test-helpers/mock-provider";
+
+export default {
+  title: "Pages/Position Types",
+};
+
+export const Basic = () => {
+  const Provider = mockProvider({
+    mocks: {
+      Query: () => ({
+        positionType: () => ({
+          all: [
+            {
+              id: 1000,
+              name: "Math Teacher",
+              externalId: "Math-12345235",
+              forPermanentPositions: true,
+              forStaffAugmentation: false,
+            },
+            {
+              id: 1001,
+              name: "Gym Teacher",
+              externalId: "Gym-2321342",
+              forPermanentPositions: true,
+              forStaffAugmentation: true,
+            },
+          ],
+        }),
+      }),
+    },
+  });
+  return (
+    <Provider>
+      <PositionTypePage />
+    </Provider>
+  );
+};
+Basic.story = {
+  name: "List View",
+};
+
+export const NoResults = () => {
+  const Provider = mockProvider({
+    mocks: {
+      Query: () => ({
+        positionType: () => ({
+          all: [],
+        }),
+      }),
+    },
+  });
+  return (
+    <Provider>
+      <PositionTypePage />
+    </Provider>
+  );
+};
+NoResults.story = {
+  name: "No Results",
+};
