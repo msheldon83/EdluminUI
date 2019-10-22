aae5842d27349f85840aaf05d657045d0bfe8c0b
Allen Dzedzy <adzedzy@edustaff.org>
2019-10-22 14:29:28 -0400

    Role Switcher added. Needs a few tweaks to clean up

--- modules/ui/app-chrome/role-switcher/index.tsx
+++ modules/ui/app-chrome/role-switcher/index.tsx
@@ -1,27 +1,77 @@
 import * as React from "react";
 import { RoleSwitcherUI } from "./role-switcher-ui";
-import { useQueryBundle } from "../../../graphql/hooks";
-import { QueryOrgUserRoles } from "ui/role-switcher/QueryOrgUserRoles.gen";
+import { some } from "lodash-es";
+import { useQueryBundle } from "graphql/hooks";
+import { QueryOrgUserRoles } from "ui/app-chrome/role-switcher/QueryOrgUserRoles.gen";
 
-type Props = {
-  roles: string[];
-};
+type Props = {};
 
-// //GQL query
 export const RoleSwitcher: React.FC<Props> = props => {
-  const OrgUserRoles = useQueryBundle(QueryOrgUserRoles);
-  OrgUserRoles.props.roles;
+  const orgUserQuery = useQueryBundle(QueryOrgUserRoles, {
+    fetchPolicy: "cache-and-network",
+  });
 
-  const orgUserRoles = {
-    isAdmin: props.roles,
-    isEmployee: props.roles,
-    isReplacementEmployee: props.roles,
+  if (
+    orgUserQuery.state === "LOADING" ||
+    !orgUserQuery.data.userAccess ||
+    !orgUserQuery.data.userAccess.me ||
+    !orgUserQuery.data.userAccess.me.user
+  ) {
+    return <></>;
+  }
+
+  const isSystemAdministrator =
+    orgUserQuery.data.userAccess.me.isSystemAdministrator;
+
+  if (isSystemAdministrator) {
+    return <></>;
+  }
+
+  if (!orgUserQuery.data.userAccess.me.user.orgUsers) {
+    return <></>;
+  }
+
+  const orgUserRoles = orgUserQuery.data.userAccess.me.user.orgUsers;
+
+  //Find elidgable roles in the orgUserRoles array
+  const roles = {
+    isAdmin: some(orgUserRoles, r => r.isAdmin),
+    isEmployee: some(orgUserRoles, r => r.isEmployee),
+    isReplacementEmployee: some(orgUserRoles, r => r.isReplacementEmployee),
   };
 
-  return (
-    <RoleSwitcherUI
-      selectedRole="Administrator"
-      roleOptions={["Administrator", "Employee", "Replacement Employee"]}
-    />
-  );
+  if (roles.isAdmin && roles.isEmployee && roles.isReplacementEmployee) {
+    return (
+      <RoleSwitcherUI
+        selectedRole="Administrator"
+        roleOptions={["Administrator", "Employee", "Replacement Employee"]}
+      />
+    );
+  }
+  if (roles.isAdmin && roles.isEmployee) {
+    return (
+      <RoleSwitcherUI
+        selectedRole="Administrator"
+        roleOptions={["Administrator", "Employee"]}
+      />
+    );
+  }
+  if (roles.isAdmin && roles.isReplacementEmployee) {
+    return (
+      <RoleSwitcherUI
+        selectedRole="Administrator"
+        roleOptions={["Administrator", "Replacement Employee"]}
+      />
+    );
+  }
+  if (roles.isEmployee && roles.isReplacementEmployee) {
+    return (
+      <RoleSwitcherUI
+        selectedRole="Employee"
+        roleOptions={["Employee", "Replacement Employee"]}
+      />
+    );
+  } else {
+    return <></>;
+  }
 };
