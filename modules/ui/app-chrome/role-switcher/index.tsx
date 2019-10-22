ec8017f9b705849cd84be9172b8d3a012e8fa0c4
Allen Dzedzy <adzedzy@edustaff.org>
2019-10-22 17:51:44 -0400

    Tweaks to account for Org Roles

--- modules/ui/app-chrome/role-switcher/index.tsx
+++ modules/ui/app-chrome/role-switcher/index.tsx
@@ -3,7 +3,7 @@ import { RoleSwitcherUI } from "./role-switcher-ui";
 import { some } from "lodash-es";
 import { useQueryBundle } from "graphql/hooks";
 import { QueryOrgUserRoles } from "ui/app-chrome/role-switcher/QueryOrgUserRoles.gen";
-import { oc } from 'ts-optchain';
+import { oc } from "ts-optchain";
 
 type Props = {};
 
@@ -16,13 +16,17 @@ export const RoleSwitcher: React.FC<Props> = props => {
     return <></>;
   }
 
-  const userAccess = oc(orgUserQuery).data.userAccess.me;
+  //OC needs to be reviewed
+  const userAccessOc = oc(orgUserQuery).data.userAccess.me;
+  const orgUserRoles = orgUserQuery.data.userAccess.me.user.orgUsers;
+  const isSystemAdministrator =
+    orgUserQuery.data.userAccess.me.isSystemAdministrator;
 
   //Find elidgible roles in the orgUserRoles array
   const roles = {
-    isAdmin: some(userAccess.user.orgUsers, 'isAdmin'),
-    isEmployee: some(userAccess.user.orgUsers, 'isEmployee'),
-    isReplacementEmployee: some(userAccess.user.orgUsers, 'isReplacementEmployee'),
+    isAdmin: some(orgUserRoles, r => r.isAdmin),
+    isEmployee: some(orgUserRoles, r => r.isEmployee),
+    isReplacementEmployee: some(orgUserRoles, r => r.isReplacementEmployee),
   };
 
   const roleOptions: string[] = [];
@@ -30,18 +34,22 @@ export const RoleSwitcher: React.FC<Props> = props => {
   roles.isEmployee && roleOptions.push("Employee");
   roles.isReplacementEmployee && roleOptions.push("Subsitute");
 
+  console.log(isSystemAdministrator);
+  console.log(roles);
+
   // Not showing the role switcher if the user is a system admin, or only has one role
-  if (userAccess.isSystemAdministrator || roleOptions.length === 1) {
+  if (isSystemAdministrator || roleOptions.length === 1) {
     return <></>;
-  }  
+  }
 
   // Default selected Role is Admin followed by sub.  Technically Employee would never be the default
-  const selectedRole = roles.isAdmin ? "Administrator" : roles.isReplacementEmployee ? "Substitute" : "Employee";
+  const selectedRole = roles.isAdmin
+    ? "Administrator"
+    : roles.isReplacementEmployee
+    ? "Substitute"
+    : "Employee";
 
   return (
-    <RoleSwitcherUI
-      selectedRole={selectedRole}
-      roleOptions={roleOptions}
-    />
-  );  
+    <RoleSwitcherUI selectedRole={selectedRole} roleOptions={roleOptions} />
+  );
 };
