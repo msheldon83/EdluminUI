51f009aacbb91cba3a6b133bf3b64e223264987b
Chris Farber <chris.farber@atomicobject.com>
2019-10-22 16:12:35 -0400

    Disable lint rule in components/table

--- modules/ui/components/table.tsx
+++ modules/ui/components/table.tsx
@@ -34,6 +34,9 @@ type Column = {
   field: string;
 };
 
+/* cf 2019-10-22 - this lint warning isn't helpful here, as these are icons: */
+/* eslint-disable react/display-name */
+
 const tableIcons: Icons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
   Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
