d162fa9ddbcb96e73ed6fa2cee8c60c90218a02a
Frank Dzedzy <fdzedzy@edustaff.org>
2019-10-23 12:13:54 -0400

    Add org switching to page

--- modules/ui/pages/organizations/index.tsx
+++ modules/ui/pages/organizations/index.tsx
@@ -1,38 +1,80 @@
 import { useQueryBundle } from "graphql/hooks";
 import { useTranslation } from "react-i18next";
 import { AllOrganizations } from "ui/pages/organizations/AllOrganizations.gen";
+import { Organization } from "graphql/server-types.gen";
 import * as React from "react";
 import { Table } from "ui/components/table";
 import { PageTitle } from "ui/components/page-title";
 import { oc } from 'ts-optchain';
+import { GetOrgsForUser } from "ui/pages/organizations/GetOrgsForUser.gen";
+import { Link } from "react-router-dom";
 
 type Props = {};
 export const OrganizationsPage: React.FC<Props> = props => {
   const { t } = useTranslation();
-  const getOrganizations = useQueryBundle(AllOrganizations, {
-    variables: { limit: 25, offset: 0 },
-  });
-
+  
   const columns = [
-    { title: t("OrgId"), field: "id", defaultSort: "asc" },
-    { title: t("Name"), field: "name" }
+    { title: t("OrgId"), field: "id" },
+    { title: t("Name"), field: "name", defaultSort: "asc"},
+    { title: "",
+      field: "actions",
+      sorting: false,
+      render: (rowData: Organization) => {
+        const switchOrg = () => {
+          return (
+            <Link to={`/admin/${rowData.id}`} >{t("Switch")}</Link>
+          );
+        };  
+        return switchOrg();
+      },
+    },
+    { title: "",
+      field: "actions",
+      sorting: false,
+      render: (rowData: Organization) => {
+        const switchOrg = () => {
+          return (
+            <Link to={`/admin/${rowData.id}`} target="_blank">{t("Switch To New Tab")}</Link>
+          );
+        };  
+        return switchOrg();
+      },
+    }
   ];
 
-  if (getOrganizations.state === "LOADING") {
+  const orgUserQuery = useQueryBundle(GetOrgsForUser, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  const getOrganizations = useQueryBundle(AllOrganizations, {
+    variables: { limit: 25, offset: 0 },  //TODO Figure out paging
+    fetchPolicy: "cache-and-network",
+  });
+  
+  if (getOrganizations.state === "LOADING" || orgUserQuery.state === "LOADING") {
     return <></>;
   }
 
-  const organizations = oc(getOrganizations).data.organization.paged.results([]);
-  const organizationsCount = organizations.length;
+  const isSystemAdministrator = oc(orgUserQuery).data.userAccess.me.isSystemAdministrator();
+  const orgUsers = oc(orgUserQuery).data.userAccess.me.user.orgUsers([]);
+  const isAdminInOrgs = orgUsers.filter(r => r && r.isAdmin);
+  let organizations = isAdminInOrgs.map(r => r && r.organization);
+  let organizationsCount = organizations.length;
+
+  if (isSystemAdministrator) {
+    organizations = oc(getOrganizations).data.organization.paged.results([]);
+    organizationsCount = oc(getOrganizations).data.organization.paged.totalCount(0);
+  } 
 
   return (
     <>
-      <PageTitle title={`${organizationsCount} ${t("Organizations")}`} />
+      <PageTitle title={t("Organizations")} />
       <Table
-        title={""}
+        title={`${organizationsCount} Records`}
         columns={columns}
         data={organizations}
-        selection={false}
+        selection={true}
+        paging={true}
       ></Table>
     </>
   );
