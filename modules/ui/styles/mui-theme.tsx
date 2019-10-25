55a7b6738078d447e456fb7846cb8d26b392604f
David Nawn <dnawn@edustaff.org>
2019-10-25 11:58:29 -0400

    Update the MUI Theme for heading tags to match the Typography doc

--- modules/ui/styles/mui-theme.tsx
+++ modules/ui/styles/mui-theme.tsx
@@ -91,12 +91,12 @@ export const EdluminTheme = createMuiTheme({
       },
 
       outlined: {
-        color: themeColors.blue,
+        color: themeColors.edluminSlate,
         textTransform: "uppercase",
-        borderColor: themeColors.blue,
-        borderWidth: pxToRem(2),
+        borderColor: themeColors.edluminLightSlate,
+        borderWidth: pxToRem(1),
         "&$disabled": {
-          borderWidth: pxToRem(2),
+          borderWidth: pxToRem(1),
         },
       },
       root: {
@@ -197,23 +197,31 @@ export const EdluminTheme = createMuiTheme({
       },
     },
     h1: {
-      fontSize: pxToRem(36),
-      fontWeight: fontWeightMedium,
+      fontSize: pxToRem(48),
+      fontWeight: "bold",
       [breakpoints.down("sm")]: {
         fontSize: pxToRem(30),
       },
     },
     h2: {
-      fontSize: pxToRem(20),
-      fontWeight: "bold",
+      fontSize: pxToRem(44),
       [breakpoints.down("sm")]: {
         fontSize: pxToRem(18),
       },
     },
     h3: {
-      fontSize: pxToRem(14),
+      fontSize: pxToRem(38),
+    },
+    h4: {
+      fontSize: pxToRem(32),
       fontWeight: "bold",
-      color: themeColors.eduBlue,
+    },
+    h5: {
+      fontSize: pxToRem(24),
+    },
+    h6: {
+      fontSize: pxToRem(16),
+      fontWeight: "bold",
     },
   }),
 });
