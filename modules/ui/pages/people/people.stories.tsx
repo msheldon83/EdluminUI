a4dd14cb748f1ae3d331c7dcbe87ae530dba1b68
Meredith Lind <meredith.lind@atomicobject.com>
2019-10-28 10:32:20 -0400

    add story for people page

---
+++ modules/ui/pages/people/people.stories.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { mockProvider } from "test-helpers/mock-provider";
+import { PeoplePage } from ".";
+
+export default {
+  title: "Pages/People",
+};
+
+export const PeopleList = () => {
+  const Provider = mockProvider({
+    mocks: {
+      Query: () => ({
+        orgUser: () => ({
+          paged: () => ({
+            totalCount: 3,
+            results: [
+              {
+                id: "100",
+                firstName: "Jane",
+                lastName: "Edwards",
+              },
+              {
+                id: "101",
+                firstName: "John",
+                lastName: "Doe",
+              },
+              {
+                id: "102",
+                firstName: "Melanie",
+                lastName: "Persons",
+              },
+            ],
+          }),
+        }),
+      }),
+    },
+  });
+
+  return (
+    <Provider>
+      <PeoplePage />
+    </Provider>
+  );
+};
+PeopleList.story = {
+  name: "List View",
+};
