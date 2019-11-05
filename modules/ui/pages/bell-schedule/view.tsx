ea333dd4fa994cc34b3ff25f6c7990d7396b171d
Frank Dzedzy <fdzedzy@edustaff.org>
2019-11-05 15:04:31 -0500

    Basic view page inactivate and delete a bell schedule

---
+++ modules/ui/pages/bell-schedule/view.tsx
@@ -0,0 +1,181 @@
+import { useQueryBundle, useMutationBundle } from "graphql/hooks";
+import { useTranslation } from "react-i18next";
+import { useScreenSize } from "hooks";
+import { Typography } from "@material-ui/core";
+import { GetWorkDayScheduleById } from "ui/pages/bell-schedule/graphql/workday-schedule.gen";
+import * as React from "react";
+import { PageTitle } from "ui/components/page-title";
+import { Section } from "ui/components/section";
+import { SectionHeader } from "ui/components/section-header";
+import { makeStyles, Grid } from "@material-ui/core";
+import { minutesToHours, boolToDisplay } from "ui/components/helpers";
+import { getDisplayName } from "ui/components/enumHelpers";
+import { Redirect, useHistory } from "react-router";
+import {
+  BellScheduleRoute,
+  BellScheduleViewRoute
+} from "ui/routes/bell-schedule";
+import { useRouteParams } from "ui/routes/definition";
+import { useState } from "react";
+import * as yup from "yup";
+import { UpdateWorkDaySchedule } from "./graphql/update-workday-schedule.gen";
+import { PageHeader } from "ui/components/page-header";
+import { DeleteWorkDaySchedule } from "./graphql/delete-workday-schedule.gen";
+import Maybe from "graphql/tsutils/Maybe";
+import { RegularSchedule } from "./components/regular-schedule";
+
+const editableSections = {
+  name: "edit-name",
+  externalId: "edit-external-id",
+};
+
+export const BellScheduleViewPage: React.FC<{}> = props => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+  const isMobile = useScreenSize() === "mobile";
+  const history = useHistory();
+  const params = useRouteParams(BellScheduleViewRoute);
+  const [editing, setEditing] = useState<string | null>(null);
+  const [enabled, setEnabled] = useState<boolean | null>(null);
+
+  const [deleteWorkDayScheduleMutation] = useMutationBundle(DeleteWorkDaySchedule);
+  const deleteWorkDaySchedule = React.useCallback(() => {
+    history.push(BellScheduleRoute.generate(params));
+    return deleteWorkDayScheduleMutation({
+      variables: {
+        workDayScheduleId: Number(params.workDayScheduleId),
+      },
+    });
+  }, [deleteWorkDayScheduleMutation, history, params]);
+
+  const [updateWorkDaySchedule] = useMutationBundle(UpdateWorkDaySchedule);
+  const enableDisableWorkDaySchedule = React.useCallback(
+    (enabled: boolean, rowVersion: string) => {
+      return updateWorkDaySchedule({
+        variables: {
+          workDaySchedule: {
+            id: Number(params.workDayScheduleId),
+            rowVersion: rowVersion,
+            expired: !enabled,
+          },
+        },
+      });
+    },
+    [updateWorkDaySchedule, params]
+  );
+
+  const getWorkDaySchedule = useQueryBundle(GetWorkDayScheduleById, {
+    variables: { id: params.workDayScheduleId },
+  });
+
+  if (getWorkDaySchedule.state === "LOADING") {
+    return <></>;
+  }
+
+  const workDaySchedule = getWorkDaySchedule?.data?.workDaySchedule?.byId;
+  if (!workDaySchedule) {
+    // Redirect the User back to the List page
+    const listUrl = BellScheduleRoute.generate(params);
+    return <Redirect to={listUrl} />;
+  }
+
+  if (enabled === null) {
+    setEnabled(!workDaySchedule.expired);
+  }
+
+  const updateName = async (name: string) => {
+    await updateWorkDaySchedule({
+      variables: {
+        workDaySchedule: {
+          id: Number(workDaySchedule.id),
+          rowVersion: workDaySchedule.rowVersion,
+          name,
+        },
+      },
+    });
+  };
+
+  const updateExternalId = async (externalId?: string | null) => {
+    await updateWorkDaySchedule({
+      variables: {
+        workDaySchedule: {
+          id: Number(workDaySchedule.id),
+          rowVersion: workDaySchedule.rowVersion,
+          externalId,
+        },
+      },
+    });
+  };
+
+  return (
+    <>
+      <PageTitle title={t("Bell Schedule")} withoutHeading={!isMobile} />
+      <PageHeader
+        text={workDaySchedule.name}
+        label={t("Name")}
+        editable={editing === null}
+        onEdit={() => setEditing(editableSections.name)}
+        validationSchema={yup.object().shape({
+          value: yup.string().required(t("Name is required")),
+        })}
+        onSubmit={async (value: Maybe<string>) => {
+          await updateName(value!);
+          setEditing(null);
+        }}
+        onCancel={() => setEditing(null)}
+        actions={[
+          {
+            name: t("Change History"),
+            onClick: () => {},
+          },
+          {
+            name: enabled ? t("Inactivate") : t("Activate"),
+            onClick: async () => {
+              await enableDisableWorkDaySchedule(
+                !enabled,
+                workDaySchedule.rowVersion
+              );
+              setEnabled(!enabled);
+            },
+          },
+          {
+            name: t("Delete"),
+            onClick: deleteWorkDaySchedule,
+          },
+        ]}
+        isInactive={!enabled}
+        inactiveDisplayText={t("This bell schedule is currently inactive.")}
+        onActivate={async () => {
+          await enableDisableWorkDaySchedule(true, workDaySchedule.rowVersion);
+          setEnabled(true);
+        }}
+      />
+      <PageHeader
+        text={workDaySchedule.externalId}
+        label={t("External ID")}
+        editable={editing === null}
+        onEdit={() => setEditing(editableSections.externalId)}
+        validationSchema={yup.object().shape({
+          value: yup.string().nullable(),
+        })}
+        onSubmit={async (value: Maybe<string>) => {
+          await updateExternalId(value);
+          setEditing(null);
+        }}
+        onCancel={() => setEditing(null)}
+        isSubHeader={true}
+        showLabel={true}
+      />
+      <Section>
+
+      </Section>
+    </>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  valueMissing: {
+    opacity: "0.6",
+    filter: "alpha(opacity = 60)",
+  },
+}));
--- modules/ui/routes/bell-schedule.tsx
+++ modules/ui/routes/bell-schedule.tsx
@@ -1,5 +1,4 @@
 import { asyncComponent } from "ui/async-component";
-import * as React from "react";
 import { AdminChromeRoute } from "./app-chrome";
 import { defineSubRoute } from "./definition";
 
@@ -16,6 +15,22 @@ export const BellScheduleLoader = asyncComponent({
     return BellSchedulePage;
   },
   name: "BellSchedule",
+});
+
+// View
+export const BellScheduleViewRoute = defineSubRoute(
+  BellScheduleRoute,
+  "/:workDayScheduleId",
+  ["workDayScheduleId"]
+);
+
+export const BellScheduleViewLoader = asyncComponent({
+  resolve: async () => {
+    const BellScheduleViewPage = (await import("ui/pages/bell-schedule/view"))
+      .BellScheduleViewPage;
+    return BellScheduleViewPage;
+  },
+  name: "BellScheduleViewPage",
 });
 
 // Add
