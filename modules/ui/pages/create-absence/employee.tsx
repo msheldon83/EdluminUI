7070240083ed4ce54f02cc4bb680b95493a397fb
Chris Farber <chris.farber@atomicobject.com>
2019-11-12 10:48:03 -0500

    employee query for create absence

--- modules/ui/pages/create-absence/employee.tsx
+++ modules/ui/pages/create-absence/employee.tsx
@@ -1,11 +1,42 @@
+import { useQueryBundle } from "graphql/hooks";
 import * as React from "react";
+import { LoadingStateTrigger } from "ui/components/loading-state/loading-state-trigger";
+import {
+  FindEmployeeForCurrentUser,
+  FindEmployeeForCurrentUserQueryResult,
+} from "./graphql/find-employee-for-current-user.gen";
 import { CreateAbsenceUI } from "./ui";
+import { flatMap, map, compact } from "lodash-es";
 
 type Props = {};
 
 export const EmployeeCreateAbsence: React.FC<Props> = props => {
-  /* TODO find the employee ID and organization ID and pass this in to the UI */
+  const potentialEmployees = useQueryBundle(FindEmployeeForCurrentUser, {
+    fetchPolicy: "cache-first",
+  });
+  if (
+    potentialEmployees.state !== "DONE" &&
+    potentialEmployees.state !== "UPDATING"
+  ) {
+    return <LoadingStateTrigger />;
+  }
+
+  const employee = findEmployee(potentialEmployees);
+  if (!employee) {
+    throw new Error("The user is not an employee");
+  }
+
   return (
-    <CreateAbsenceUI employeeId="xxx" actingAsEmployee organizationId="123" />
+    <CreateAbsenceUI
+      employeeId={employee.id}
+      actingAsEmployee
+      organizationId={employee.orgId.toString()}
+    />
   );
+};
+
+const findEmployee = (data: FindEmployeeForCurrentUserQueryResult) => {
+  const orgUsers = data.data?.userAccess?.me?.user?.orgUsers ?? [];
+  const emps = compact(map(orgUsers, u => u?.employee));
+  return emps[0];
 };
