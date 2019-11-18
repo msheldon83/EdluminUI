6752a2720721494257396fff01140f73762dd9a2
Chris Farber <chris.farber@atomicobject.com>
2019-11-18 12:35:34 -0500

    Remove org home route

--- modules/ui/pages/organization-home/loader.tsx
+++
@@ -1,8 +0,0 @@
-import { asyncComponent } from "ui/async-component";
-
-export const OrganizationHomeLoader = asyncComponent({
-  async resolve() {
-    return (await import("./index")).OrganizationsHome;
-  },
-  name: "Organization Home",
-});
