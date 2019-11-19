19ae5a5c0e1b66028482eb4e27c929c61e724440
David Nawn <dnawn@edustaff.org>
2019-11-19 09:52:14 -0500

    Adding in Confirmation screen stories and getting the page looking right

--- modules/ui/pages/create-absence/confirmation.stories.tsx
+++ modules/ui/pages/create-absence/confirmation.stories.tsx
@@ -2,12 +2,157 @@ import * as React from "react";
 import { mockProvider } from "test-helpers/mock-provider";
 import { AdminCreateAbsenceRoute } from "ui/routes/create-absence";
 import { Confirmation } from "./confirmation";
+import { VacancyDetail, Maybe } from "graphql/server-types.gen";
 
 export default {
   title: "Pages/Create Absence/Confirmation",
 };
 
-export const AsAdminBasic = () => {
+const vacancies = [
+  {
+    startTimeLocal: new Date("11/1/2019 08:00 AM"),
+    endTimeLocal: new Date("11/3/2019 05:00 PM"),
+    numDays: 3,
+    positionId: 1,
+    details: [
+      {
+        startTimeLocal: new Date("11/1/2019 07:00 AM"),
+        endTimeLocal: new Date("11/1/2019 09:00 AM"),
+        locationId: 1,
+        location: {
+          name: "Evans Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/1/2019 09:00 AM"),
+        endTimeLocal: new Date("11/1/2019 12:00 PM"),
+        locationId: 2,
+        location: {
+          name: "Brook Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/1/2019 01:00 PM"),
+        endTimeLocal: new Date("11/1/2019 05:00 PM"),
+        locationId: 3,
+        location: {
+          name: "Haven Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/2/2019 07:00 AM"),
+        endTimeLocal: new Date("11/2/2019 09:00 AM"),
+        locationId: 1,
+        location: {
+          name: "Evans Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/2/2019 09:00 AM"),
+        endTimeLocal: new Date("11/2/2019 12:00 PM"),
+        locationId: 2,
+        location: {
+          name: "Brook Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/2/2019 01:00 PM"),
+        endTimeLocal: new Date("11/2/2019 05:00 PM"),
+        locationId: 3,
+        location: {
+          name: "Haven Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/3/2019 07:00 AM"),
+        endTimeLocal: new Date("11/3/2019 09:00 AM"),
+        locationId: 1,
+        location: {
+          name: "Evans Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/3/2019 09:00 AM"),
+        endTimeLocal: new Date("11/3/2019 12:00 PM"),
+        locationId: 2,
+        location: {
+          name: "Brook Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/3/2019 01:00 PM"),
+        endTimeLocal: new Date("11/3/2019 05:00 PM"),
+        locationId: 3,
+        location: {
+          name: "Haven Elementary School",
+        },
+      },
+    ] as Maybe<VacancyDetail[]>,
+  },
+  {
+    startTimeLocal: new Date("11/6/2019 08:00 AM"),
+    endTimeLocal: new Date("11/7/2019 05:00 PM"),
+    numDays: 2,
+    positionId: 1,
+    details: [
+      {
+        startTimeLocal: new Date("11/6/2019 07:00 AM"),
+        endTimeLocal: new Date("11/6/2019 09:00 AM"),
+        locationId: 1,
+        location: {
+          name: "Evans Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/6/2019 09:00 AM"),
+        endTimeLocal: new Date("11/6/2019 12:00 PM"),
+        locationId: 2,
+        location: {
+          name: "Brook Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/6/2019 01:00 PM"),
+        endTimeLocal: new Date("11/6/2019 05:00 PM"),
+        locationId: 3,
+        location: {
+          name: "Haven Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/7/2019 07:00 AM"),
+        endTimeLocal: new Date("11/7/2019 09:00 AM"),
+        locationId: 1,
+        location: {
+          name: "Evans Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/7/2019 09:00 AM"),
+        endTimeLocal: new Date("11/7/2019 12:00 PM"),
+        locationId: 2,
+        location: {
+          name: "Brook Elementary School",
+        },
+      },
+      {
+        startTimeLocal: new Date("11/7/2019 01:00 PM"),
+        endTimeLocal: new Date("11/7/2019 05:00 PM"),
+        locationId: 3,
+        location: {
+          name: "Haven Elementary School",
+        },
+      },
+    ] as Maybe<VacancyDetail[]>,
+  },
+];
+
+const notesToApprover =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore";
+const notesToSubstitute =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+export const AsAdminWithAllInformation = () => {
   const path = AdminCreateAbsenceRoute.generate({
     organizationId: "1006",
     employeeId: "123",
@@ -16,9 +161,43 @@ export const AsAdminBasic = () => {
     initialUrl: path,
     mocks: {},
   });
+
   return (
     <Provider>
-      <Confirmation absenceId={"123456789"} dispatch={() => {}} />
+      <Confirmation
+        absenceId={"123456789"}
+        totalNumberOfDays={10}
+        vacancies={vacancies}
+        needsReplacement={true}
+        notesToApprover={notesToApprover}
+        absenceReasonName={"Illness"}
+        notesToSubstitute={notesToSubstitute}
+        preAssignedReplacementEmployeeName={"Luke Skywalker"}
+        dispatch={() => {}}
+      />
+    </Provider>
+  );
+};
+
+export const AsAdminWithMinimumInformation = () => {
+  const path = AdminCreateAbsenceRoute.generate({
+    organizationId: "1006",
+    employeeId: "123",
+  });
+  const Provider = mockProvider({
+    initialUrl: path,
+    mocks: {},
+  });
+
+  return (
+    <Provider>
+      <Confirmation
+        absenceId={"123456789"}
+        totalNumberOfDays={10}
+        needsReplacement={false}
+        absenceReasonName={"Illness"}
+        dispatch={() => {}}
+      />
     </Provider>
   );
 };
--- modules/ui/pages/create-absence/confirmation.tsx
+++ modules/ui/pages/create-absence/confirmation.tsx
@@ -1,17 +1,33 @@
 import * as React from "react";
 import { Vacancy } from "graphql/server-types.gen";
-import { Grid, makeStyles } from "@material-ui/core";
+import {
+  Grid,
+  makeStyles,
+  Typography,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Chip,
+} from "@material-ui/core";
 import { useScreenSize } from "hooks";
 import { useTranslation } from "react-i18next";
 import { CreateAbsenceActions } from "./state";
+import { Section } from "ui/components/section";
+import { VacancyDetails } from "./vacancy-details";
 
 type Props = {
   absenceId: string | undefined;
+  totalNumberOfDays: number;
   dispatch: React.Dispatch<CreateAbsenceActions>;
   vacancies?: Pick<
     Vacancy,
     "startTimeLocal" | "endTimeLocal" | "numDays" | "positionId" | "details"
   >[];
+  needsReplacement: boolean;
+  absenceReasonName: string;
+  notesToApprover?: string;
+  notesToSubstitute?: string;
+  preAssignedReplacementEmployeeName?: string;
 };
 
 export const Confirmation: React.FC<Props> = props => {
@@ -27,20 +43,101 @@ export const Confirmation: React.FC<Props> = props => {
     });
   }
 
+  const hasVacancies = props.vacancies && props.vacancies.length;
+
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <div className={classes.confirmationBanner}>
-          <div>
-            {t("Your absence has been saved. We'll take it from here.")}
+    <Section>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <div className={classes.confirmationBanner}>
+            <div>
+              {t("Your absence has been saved. We'll take it from here.")}
+            </div>
+            <Typography variant="h1" className={classes.confirmationText}>
+              {`${t("Confirmation #")} ${props.absenceId}`}
+            </Typography>
           </div>
-          <div>{`${t("Confirmation #")} ${props.absenceId}`}</div>
-        </div>
+        </Grid>
+        {hasVacancies && (
+          <Grid item xs={5}>
+            <Section className={classes.vacancyDetailsSection}>
+              <VacancyDetails
+                vacancies={props.vacancies ?? []}
+                equalWidthDetails
+              />
+              <div className={classes.requiresSubSection}>
+                <FormControlLabel
+                  label={t("Requires a substitute")}
+                  control={
+                    <Checkbox
+                      checked={props.needsReplacement}
+                      disabled={true}
+                    />
+                  }
+                />
+              </div>
+              {props.preAssignedReplacementEmployeeName && (
+                <div className={classes.preArrangedChip}>
+                  <Chip
+                    label={`${t("Pre-arranged")}: ${
+                      props.preAssignedReplacementEmployeeName
+                    }`}
+                    color={"primary"}
+                  />
+                </div>
+              )}
+            </Section>
+            <div className={classes.notesForSubSection}>
+              <Typography variant={"h6"}>
+                {t("Notes for substitute")}:
+              </Typography>
+              <Typography className={classes.subText}>
+                {t(
+                  "Can be seen by the administrator and the employee as well as the assigned substitute."
+                )}
+              </Typography>
+              <div className={classes.notesForSub}>
+                {props.notesToSubstitute || (
+                  <span className={classes.valueMissing}>
+                    {t("No Notes Specified")}
+                  </span>
+                )}
+              </div>
+            </div>
+          </Grid>
+        )}
+        <Grid item xs={hasVacancies ? 7 : 12}>
+          <Typography variant={"h6"}>{t("Reason")}:</Typography>
+          <div>
+            {`${props.absenceReasonName} (${props.totalNumberOfDays} ${
+              props.totalNumberOfDays === 1 ? t("day") : t("days")
+            })`}
+          </div>
+          <div className={classes.notesToApproverSection}>
+            <Typography variant={"h6"}>{t("Notes")}:</Typography>
+            <div>
+              {props.notesToApprover || (
+                <span className={classes.valueMissing}>
+                  {t("No Notes Specified")}
+                </span>
+              )}
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} container justify="flex-end" spacing={2}>
+          <Grid item>
+            <Button variant="outlined" color="primary" onClick={() => {}}>
+              {t("Create New")}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="outlined" color="primary" onClick={() => {}}>
+              {t("Back to List")}
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        Buttons here
-      </Grid>
-    </Grid>
+    </Section>
   );
 };
 
@@ -49,5 +146,38 @@ const useStyles = makeStyles(theme => ({
     textAlign: "center",
     color: theme.customColors.white,
     backgroundColor: "#099E47",
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  confirmationText: {
+    marginTop: theme.spacing(2),
+  },
+  vacancyDetailsSection: {
+    padding: theme.spacing(2),
+  },
+  notesToApproverSection: {
+    marginTop: theme.spacing(2),
+  },
+  requiresSubSection: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  preArrangedChip: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  notesForSubSection: {
+    marginTop: theme.spacing(4),
+  },
+  notesForSub: {
+    marginTop: theme.spacing(),
+  },
+  subText: {
+    color: theme.customColors.darkGray,
+  },
+  valueMissing: {
+    fontWeight: "normal",
+    opacity: "0.6",
+    filter: "alpha(opacity = 60)",
   },
 }));
