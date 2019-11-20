ba19eeda4852aa104e1d08ba49da551b49dfb8ca
Frank Dzedzy <fdzedzy@edustaff.org>
2019-11-19 21:07:11 -0500

    dismiss absence

--- modules/ui/pages/sub-home/index.tsx
+++ modules/ui/pages/sub-home/index.tsx
@@ -9,9 +9,11 @@ import { SectionHeader } from "ui/components/section-header";
 import { Filters } from "./filters/index";
 import { AvailableJob } from "./components/available-job";
 import { FilterList } from "@material-ui/icons";
-import { useQueryBundle } from "graphql/hooks";
+import { useQueryBundle, useMutationBundle } from "graphql/hooks";
 import { GetAllVacancies } from "./graphql/get-all-vacancies.gen";
-import { Vacancy } from "graphql/server-types.gen";
+import { DismissVacancy } from "./graphql/dismiss-vacancy.gen";
+import { Vacancy, OrgUser } from "graphql/server-types.gen";
+import { QueryOrgUsers } from "./graphql/get-orgusers.gen";
 
 type Props = {};
 
@@ -22,6 +24,8 @@ export const SubHome: React.FC<Props> = props => {
   const classes = useStyles();
   const isMobile = useScreenSize() === "mobile";
   const [showFilters, setShowFilters] = React.useState(!isMobile);
+  const [dismissVacancyMutation] = useMutationBundle(DismissVacancy);
+  const getOrgUsers = useQueryBundle(QueryOrgUsers);
 
   const getVacancies = useQueryBundle(GetAllVacancies, {
     variables: {},
@@ -44,6 +48,36 @@ export const SubHome: React.FC<Props> = props => {
     | "totalDayPortion"
     | "details"
   >[];
+
+  const orgUsers = (getOrgUsers.state === "LOADING" ||
+  getOrgUsers.state === "UPDATING"
+    ? []
+    : getOrgUsers.data?.userAccess?.me?.user?.orgUsers ?? []) as Pick<
+    OrgUser,
+    "id" | "orgId"
+  >[];
+
+  const onDismissVacancy = async (orgId: string, vacancyId: string) => {
+    const employeeId = determineEmployeeId(orgId);
+    if (employeeId != 0) {
+      await Promise.resolve(
+        dismissVacancyMutation({
+          variables: {
+            vacancyRejection: {
+              vacancyId: vacancyId,
+              employeeId: Number(employeeId),
+            },
+          },
+        })
+      );
+    }
+    await getVacancies.refetch();
+  };
+
+  const determineEmployeeId = (orgId: string) => {
+    const employeeId = orgUsers.find(o => o.orgId === Number(orgId))?.id ?? 0;
+    return employeeId;
+  };
 
   return (
     <>
@@ -97,8 +131,8 @@ export const SubHome: React.FC<Props> = props => {
             vacancies.map((vacancy, index) => (
               <AvailableJob
                 vacancy={vacancy}
-                shadeRow={false}
                 shadeRow={index % 2 != 0}
+                onDismiss={onDismissVacancy}
                 key={index}
               />
             ))
