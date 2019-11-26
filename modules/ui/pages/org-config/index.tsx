e1161339a4f8da6d867e060d883334a0ce529a38
Allen Dzedzy <adzedzy@edustaff.org>
2019-11-26 12:34:14 -0500

    Load missing icons

--- modules/ui/pages/org-config/index.tsx
+++ modules/ui/pages/org-config/index.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { PageTitle } from "ui/components/page-title";
+import SettingsIcon from "@material-ui/icons/Settings";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
 import { Link } from "react-router-dom";
 import { useRouteParams } from "ui/routes/definition";
 import { AdminChromeRoute } from "ui/routes/app-chrome";
 import { BellScheduleRoute } from "ui/routes/bell-schedule";
 import { PositionTypeRoute } from "ui/routes/position-type";
 import { Typography } from "@material-ui/core";
+
+//Material UI Icons
+//Grid Component for layout
+//Create Pages for Links
+//Create Routes for Pages
+//Reference for non-Material UI Icons: <img src={require("ui/icons/visibility_time.svg")} />
 
 export const OrgConfigPage: React.FC<{}> = props => {
   const { t } = useTranslation();
@@ -25,7 +34,7 @@ export const OrgConfigPage: React.FC<{}> = props => {
 
   return (
     <>
-      <PageTitle title={t("Configuration")} />
+      <PageTitle title={t("Settings")} />
       <div>
         {configRoutes.map((r, i) => {
           return (
