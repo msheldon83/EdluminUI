c844cc0402caace4e74513c92aac8c9eefc0e679
Frank Dzedzy <fdzedzy@edustaff.org>
2019-11-26 19:43:11 -0500

    refactor for home page

--- modules/ui/pages/verify/index.tsx
+++ modules/ui/pages/verify/index.tsx
@@ -2,31 +2,14 @@ import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { PageTitle } from "ui/components/page-title";
 import { Section } from "ui/components/section";
-import {
-  Checkbox,
-  FormControlLabel,
-  Grid,
-  InputLabel,
-  makeStyles,
-} from "@material-ui/core";
-import { OptionType, Select } from "ui/components/form/select";
-import { useLocations } from "reference-data/locations";
-import { useCallback, useMemo, useState } from "react";
+import { useState } from "react";
 import { Filters } from "./components/filters";
-import { DateTabs } from "./components/tabs";
+import { VerifyUI } from "./ui";
 
 export const VerifyPage: React.FC<{}> = props => {
   const { t } = useTranslation();
-  const classes = useStyles();
   const [showVerified, setShowVerified] = useState(false);
   const [locationsFilter, setLocationsFilter] = useState<number[]>([]);
-
-  const dateTabOptions = [
-    { dateLabel: "Today", count: 5 },
-    { dateLabel: "Older", count: 56 },
-  ];
-  const today = useMemo(() => new Date(), []);
-  const [selectedDateTab, setSelectedDateTab] = useState<string>("Today");
 
   return (
     <>
@@ -39,15 +22,7 @@ export const VerifyPage: React.FC<{}> = props => {
           setLocationsFilter={setLocationsFilter}
         />
       </Section>
-      <Section>
-        <DateTabs
-          selectedDateTab={selectedDateTab}
-          setSelectedDateTab={setSelectedDateTab}
-          dateOptions={dateTabOptions}
-        />
-      </Section>
+      <VerifyUI showVerified={showVerified} locationsFilter={locationsFilter} />
     </>
   );
 };
-
-export const useStyles = makeStyles(theme => ({}));
