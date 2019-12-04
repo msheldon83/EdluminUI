1c4b439c3a32b0d6df2354ff9d77b69ea224c1ca
David Nawn <dnawn@edustaff.org>
2019-12-03 21:10:23 -0500

    Swap content and salutations based on the time of day. Provide a way in dev to toggle between the different times of day for testing.

--- modules/ui/pages/admin-home/index.tsx
+++ modules/ui/pages/admin-home/index.tsx
@@ -4,9 +4,17 @@ import { useTranslation } from "react-i18next";
 import { AdminHomeRoute } from "ui/routes/admin-home";
 import { useRouteParams } from "ui/routes/definition";
 import { DailyReport } from "ui/components/reports/daily-report/daily-report";
-import { startOfToday } from "date-fns";
-import { Grid } from "@material-ui/core";
+import { startOfToday, getHours, startOfTomorrow } from "date-fns";
+import { Grid, Button, Tooltip } from "@material-ui/core";
 import { DateStepperHeader } from "ui/components/date-stepper-header";
+import { useMemo, useState, useEffect } from "react";
+import { useQueryBundle, HookQueryResult } from "graphql/hooks";
+import {
+  GetUserName,
+  GetUserNameQuery,
+  GetUserNameQueryVariables,
+} from "./graphql/get-user-name.gen";
+import { TFunction } from "i18next";
 
 type Props = {};
 
@@ -14,7 +22,31 @@ export const AdminHome: React.FC<Props> = props => {
   const classes = useStyles();
   const { t } = useTranslation();
   const params = useRouteParams(AdminHomeRoute);
-  const [date, setDate] = React.useState(startOfToday());
+  const [date, setDate] = useState(startOfToday());
+  const [timeOfDay, setTimeOfDay] = useState(getTimeOfDay());
+  const getUserName = useQueryBundle(GetUserName, {
+    fetchPolicy: "cache-first",
+  });
+  const salutation = getSalutation(timeOfDay, getUserName, t);
+
+  useEffect(() => {
+    if (timeOfDay === "morning") {
+      setDate(startOfToday());
+    }
+    if (timeOfDay === "evening") {
+      setDate(startOfTomorrow());
+    }
+  }, [timeOfDay]);
+
+  const toggleTimeOfDay = () => {
+    let newTimeOfDay: TimeOfDay = "morning";
+    if (timeOfDay === "morning") {
+      newTimeOfDay = "afternoon";
+    } else if (timeOfDay === "afternoon") {
+      newTimeOfDay = "evening";
+    }
+    setTimeOfDay(newTimeOfDay);
+  };
 
   return (
     <>
@@ -27,17 +59,20 @@ export const AdminHome: React.FC<Props> = props => {
         <Grid item>
           <DateStepperHeader date={date} setDate={setDate}></DateStepperHeader>
         </Grid>
-        <Grid item>{/* Action buttons go here */}</Grid>
+        <Grid item>{getDevViewToggle(toggleTimeOfDay, timeOfDay)}</Grid>
       </Grid>
 
-      <DailyReport
-        orgId={params.organizationId}
-        date={date}
-        setDate={setDate}
-        header={t("Good morning")}
-        showFilters={false}
-        cards={["unfilled", "total"]}
-      />
+      {(timeOfDay === "morning" || timeOfDay === "evening") && (
+        <DailyReport
+          orgId={params.organizationId}
+          date={date}
+          setDate={setDate}
+          header={salutation}
+          showFilters={false}
+          cards={["unfilled", "total"]}
+        />
+      )}
+      {timeOfDay === "afternoon" && <div>Verify goes here</div>}
     </>
   );
 };
@@ -47,3 +82,61 @@ const useStyles = makeStyles(theme => ({
     marginBottom: theme.spacing(3),
   },
 }));
+
+const getSalutation = (
+  timeOfDay: TimeOfDay,
+  queryResult: HookQueryResult<GetUserNameQuery, GetUserNameQueryVariables>,
+  t: TFunction
+) => {
+  const firstName =
+    queryResult.state === "LOADING"
+      ? undefined
+      : queryResult.data?.userAccess?.me?.user?.firstName;
+
+  let salutation = undefined;
+  switch (timeOfDay) {
+    case "morning":
+      salutation = t("Good morning");
+      break;
+    case "afternoon":
+      salutation = t("Good afternoon");
+      break;
+    case "evening":
+      salutation = t("Good evening");
+      break;
+  }
+
+  return firstName ? `${salutation}, ${firstName}` : salutation;
+};
+
+type TimeOfDay = "morning" | "afternoon" | "evening";
+
+const getTimeOfDay = (): TimeOfDay => {
+  const now = new Date();
+  const currentHour = getHours(now);
+
+  if (currentHour < 12) {
+    return "morning";
+  }
+
+  if (currentHour >= 12 && currentHour < 15) {
+    return "afternoon";
+  }
+
+  return "evening";
+};
+
+const getDevViewToggle = (
+  toggleTimeOfDay: () => void,
+  currentTimeOfDay: TimeOfDay
+) => {
+  return (
+    __DEV__ && (
+      <Tooltip title={`Currently ${currentTimeOfDay}`}>
+        <Button variant="contained" color="secondary" onClick={toggleTimeOfDay}>
+          Toggle Time Of Day
+        </Button>
+      </Tooltip>
+    )
+  );
+};
