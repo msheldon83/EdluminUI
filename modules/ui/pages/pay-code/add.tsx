ce4556edc8e3a8d6e03e75e6af50f938b2311438
Allen Dzedzy <adzedzy@edustaff.org>
2019-12-10 12:19:31 -0500

    Index page finished

---
+++ modules/ui/pages/pay-code/add.tsx
@@ -0,0 +1,50 @@
+import { makeStyles, useTheme } from "@material-ui/styles";
+import { useIsMobile } from "hooks";
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router";
+import { PageTitle } from "ui/components/page-title";
+import { SchoolGroupsRoute } from "ui/routes/school-groups";
+import { useRouteParams } from "ui/routes/definition";
+import { Button } from "@material-ui/core";
+
+type Props = {};
+
+export const SchoolGroups: React.FC<Props> = props => {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const theme = useTheme();
+  const classes = useStyles();
+  const isMobile = useIsMobile();
+  const params = useRouteParams(SchoolGroupsRoute);
+
+  const [triggerError, setTriggerError] = React.useState(false);
+
+  if (triggerError) {
+    throw Error("error!");
+  }
+
+  return (
+    <>
+      <PageTitle title={`${params.organizationId} ${t("School Groups")}`} />
+      {__DEV__ && (
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            setTriggerError(true);
+          }}
+        >
+          Trigger Error
+        </Button>
+      )}
+    </>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  filters: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+}));
