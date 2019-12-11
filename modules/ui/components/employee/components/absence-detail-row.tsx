90da3cb39c9fca2961b95e420df3936b561b70a1
David Nawn <dnawn@edustaff.org>
2019-12-11 16:16:39 -0500

    Create an AbsenceDetailRow component that can be used on the calendar view as well as the existing list component of scheduled absences

---
+++ modules/ui/components/employee/components/absence-detail-row.tsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { EmployeeAbsenceDetail } from "../types";
+import { Grid, makeStyles, Typography, Button } from "@material-ui/core";
+import { isEqual, format } from "date-fns";
+import { DayIcon } from "ui/components/day-icon";
+import { parseDayPortion } from "ui/components/helpers";
+
+type Props = {
+  absence: EmployeeAbsenceDetail;
+  cancelAbsence: (absenceId: string) => Promise<void>;
+};
+
+export const AbsenceDetailRow: React.FC<Props> = props => {
+  const { t } = useTranslation();
+  const classes = useStyles();
+
+  const dateRangeDisplay = isEqual(
+    props.absence.startDate,
+    props.absence.endDate
+  )
+    ? format(props.absence.startDate, "MMM d")
+    : `${format(props.absence.startDate, "MMM d")} - ${format(
+        props.absence.endDate,
+        "MMM d"
+      )}`;
+
+  const dayPortionNumberDisplay = Math.round(props.absence.totalDayPortion);
+  const dayPortionDisplay =
+    dayPortionNumberDisplay >= 1
+      ? `${dayPortionNumberDisplay} ${parseDayPortion(
+          t,
+          props.absence.totalDayPortion
+        )}`
+      : parseDayPortion(t, props.absence.totalDayPortion);
+
+  return (
+    <>
+      <Grid item xs={3}>
+        <div className={classes.detailText}>{props.absence.absenceReason}</div>
+        <div className={classes.subText}>{dateRangeDisplay}</div>
+      </Grid>
+      <Grid item xs={3}>
+        {props.absence.substitute && (
+          <>
+            <div className={classes.detailText}>
+              {props.absence.substitute?.name}
+            </div>
+            <div className={classes.subText}>
+              {props.absence.substitute?.phoneNumber}
+            </div>
+          </>
+        )}
+        {!props.absence.substitute && props.absence.subRequired && (
+          <div className={classes.subText}>{t("No substitute assigned")}</div>
+        )}
+        {!props.absence.substitute && !props.absence.subRequired && (
+          <div className={classes.subText}>{t("No substitute required")}</div>
+        )}
+      </Grid>
+      <Grid item xs={3}>
+        <div className={classes.dayPartContainer}>
+          <DayIcon
+            dayPortion={props.absence.totalDayPortion}
+            startTime={props.absence.startTimeLocal.toString()}
+          />
+          <div className={classes.dayPart}>
+            <div className={classes.detailText}>{dayPortionDisplay}</div>
+            <div className={classes.subText}>
+              {`${props.absence.startTime} - ${props.absence.endTime}`}
+            </div>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs={1}>
+        <div className={classes.detailText}>{`#${props.absence.id}`}</div>
+      </Grid>
+      <Grid item xs={2} className={classes.cancelButtonContainer}>
+        <Button
+          variant="outlined"
+          onClick={async () => await props.cancelAbsence(props.absence.id)}
+          className={classes.cancelButton}
+        >
+          {t("Cancel")}
+        </Button>
+      </Grid>
+    </>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  detailText: {
+    fontWeight: "bold",
+  },
+  subText: {
+    color: theme.customColors.edluminSubText,
+  },
+  dayPartContainer: {
+    display: "flex",
+  },
+  dayPart: {
+    paddingLeft: theme.spacing(),
+  },
+  cancelButtonContainer: {
+    textAlign: "right",
+  },
+  cancelButton: {
+    color: theme.palette.error.main,
+  },
+}));
--- modules/ui/components/employee/components/scheduled-absences.tsx
+++ modules/ui/components/employee/components/scheduled-absences.tsx
@@ -3,9 +3,7 @@ import { useTranslation } from "react-i18next";
 import { makeStyles, Grid, Button, Typography } from "@material-ui/core";
 import { SectionHeader } from "ui/components/section-header";
 import { EmployeeAbsenceDetail } from "ui/components/employee/types";
-import { isEqual, format } from "date-fns";
-import { DayIcon } from "ui/components/day-icon";
-import { parseDayPortion } from "ui/components/helpers";
+import { AbsenceDetailRow } from "./absence-detail-row";
 
 type Props = {
   header?: string;
@@ -45,71 +43,9 @@ export const ScheduledAbsences: React.FC<Props> = props => {
           i % 2 == 1 ? classes.shadedRow : undefined,
         ].join(" ");
 
-        const dateRangeDisplay = isEqual(a.startDate, a.endDate)
-          ? format(a.startDate, "MMM d")
-          : `${format(a.startDate, "MMM d")} - ${format(a.endDate, "MMM d")}`;
-
-        const dayPortionNumberDisplay = Math.round(a.totalDayPortion);
-        const dayPortionDisplay =
-          dayPortionNumberDisplay >= 1
-            ? `${dayPortionNumberDisplay} ${parseDayPortion(
-                t,
-                a.totalDayPortion
-              )}`
-            : parseDayPortion(t, a.totalDayPortion);
-
         return (
           <Grid item container xs={12} key={i} className={className}>
-            <Grid item xs={3}>
-              <div>{a.absenceReason}</div>
-              <div className={classes.subText}>{dateRangeDisplay}</div>
-            </Grid>
-            <Grid item xs={3}>
-              {a.substitute && (
-                <>
-                  <div>{a.substitute?.name}</div>
-                  <div className={classes.subText}>
-                    {a.substitute?.phoneNumber}
-                  </div>
-                </>
-              )}
-              {!a.substitute && a.subRequired && (
-                <div className={classes.subText}>
-                  {t("No substitute assigned")}
-                </div>
-              )}
-              {!a.substitute && !a.subRequired && (
-                <div className={classes.subText}>
-                  {t("No substitute required")}
-                </div>
-              )}
-            </Grid>
-            <Grid item xs={3}>
-              <div className={classes.dayPartContainer}>
-                <DayIcon
-                  dayPortion={a.totalDayPortion}
-                  startTime={a.startTimeLocal.toString()}
-                />
-                <div className={classes.dayPart}>
-                  <div>{dayPortionDisplay}</div>
-                  <div className={classes.subText}>
-                    {`${a.startTime} - ${a.endTime}`}
-                  </div>
-                </div>
-              </div>
-            </Grid>
-            <Grid item xs={1}>
-              <div>{`#${a.id}`}</div>
-            </Grid>
-            <Grid item xs={2} className={classes.cancelButtonContainer}>
-              <Button
-                variant="outlined"
-                onClick={async () => await props.cancelAbsence(a.id)}
-                className={classes.cancelButton}
-              >
-                {t("Cancel")}
-              </Button>
-            </Grid>
+            <AbsenceDetailRow absence={a} cancelAbsence={props.cancelAbsence} />
           </Grid>
         );
       })}
@@ -118,29 +54,12 @@ export const ScheduledAbsences: React.FC<Props> = props => {
 };
 
 const useStyles = makeStyles(theme => ({
-  subText: {
-    color: theme.customColors.edluminSubText,
-    fontWeight: "normal",
-  },
   detail: {
     padding: theme.spacing(2),
-    fontWeight: "bold",
   },
   shadedRow: {
     background: theme.customColors.lightGray,
     borderTop: `1px solid ${theme.customColors.medLightGray}`,
     borderBottom: `1px solid ${theme.customColors.medLightGray}`,
-  },
-  dayPartContainer: {
-    display: "flex",
-  },
-  dayPart: {
-    paddingLeft: theme.spacing(),
-  },
-  cancelButtonContainer: {
-    textAlign: "right",
-  },
-  cancelButton: {
-    color: theme.palette.error.main,
   },
 }));
