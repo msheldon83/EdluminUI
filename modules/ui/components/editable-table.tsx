9e5f7f82beca6bf6063493750f680a62796a91e2
Allen Dzedzy <adzedzy@edustaff.org>
2019-12-13 16:46:34 -0500

    tweak

--- modules/ui/components/editable-table.tsx
+++ modules/ui/components/editable-table.tsx
@@ -2,22 +2,15 @@ import * as React from "react";
 import { Table } from "ui/components/table";
 import { TableProps } from "@material-ui/core/Table";
 
-type Props = {
+type Props = TableProps & {
   onRowAdd: () => void;
   onRowUpdate: () => void;
   onRowDelete: () => void;
-} & TableProps;
+};
 
 export const EditableTable = (props: Props) => {
   const { onRowAdd, onRowUpdate, onRowDelete, ...tableProps } = props;
   return (
-    <Table
-      {...tableProps}
-      editable={{
-        onRowAdd,
-        onRowUpdate,
-        onRowDelete,
-      }}
-    />
+    <Table {...tableProps} editable={{ onRowAdd, onRowUpdate, onRowDelete }} />
   );
 };
