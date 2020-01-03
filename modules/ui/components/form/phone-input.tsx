9ed0c8b8544448be87fbd9c152d844be2f51fd2f
Scott Corgan <scottcorgan@gmail.com>
2020-01-03 10:03:01 -0500

    Disable more than 10 digits for phone number

--- modules/ui/components/form/phone-input.tsx
+++ modules/ui/components/form/phone-input.tsx
@@ -12,61 +12,66 @@ export const PhoneInput = (props: PhoneInputProps) => {
   const { onValidPhoneNumber = () => {}, value, ...inputProps } = props;
 
   /*
+    - pull out all numbers
+    - combine all values of that array into a single string
+  */
+  const strippedValue = value?.match(/\d+/g)?.join("") ?? "";
+
+  /*
     The validation in the handler is specific and primitive in order to accomdate only
     handling US numbers. It will have to change to add support for more countries in the future
   */
   const handleBlur = React.useCallback(
     e => {
-      const defaultBlur = () => {
-        // Don't override regular use of onBlur
-        if (typeof inputProps.onBlur === "function") {
-          inputProps.onBlur(e);
-        }
-      };
-
-      if (!value) {
-        defaultBlur();
-        return;
+      if (strippedValue.length === 10) {
+        onValidPhoneNumber(formatNumber(strippedValue));
       }
 
-      const matchedValues = value.match(/\d+/g);
-
-      if (matchedValues === null) {
-        defaultBlur();
-        return;
+      // Don't override regular use of onBlur
+      if (typeof inputProps.onBlur === "function") {
+        inputProps.onBlur(e);
       }
-
-      const number = matchedValues.map(Number).join("");
-
-      if (number.length === 10) {
-        onValidPhoneNumber(formatNumber(number));
-      }
-
-      defaultBlur();
     },
-    [value, onValidPhoneNumber, inputProps]
+    [strippedValue, onValidPhoneNumber, inputProps]
   );
 
   // Only allow valid characters
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    const INVALID_CHARACTERS = e.metaKey ? /\\/g : /[a-z]/gi;
+  const handleKeyDown = React.useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      const INVALID_CHARACTERS = e.metaKey ? /\\/g : /[a-z]/gi;
+      const isValid = e.key.length > 1 || !INVALID_CHARACTERS.test(e.key);
+      const isNumber = !isNaN(Number(e.key));
 
-    const isValid = e.key.length > 1 || !INVALID_CHARACTERS.test(e.key);
+      /*
+      If it's at 10 valid digits, it's not a special key and the meta key isn't pressed.
+      This allows for things like select all to still be available to press.
+        - is anumber
+        - has 10 characters
+        - key pressed is not alpha-numeric
+        - meta key isn't pressed
+    */
+      const atMaxLength =
+        isNumber &&
+        strippedValue.length === 10 &&
+        e.key.length === 1 &&
+        !e.metaKey;
 
-    if (isValid) {
-      return true;
-    }
+      if (isValid && !atMaxLength) {
+        return true;
+      }
 
-    e.stopPropagation();
-    e.preventDefault();
+      e.stopPropagation();
+      e.preventDefault();
 
-    return false;
-  };
+      return false;
+    },
+    [strippedValue]
+  );
 
   return (
     <Input
       {...inputProps}
-      value={value}
+      value={value === "undefined" ? "" : value}
       onBlur={handleBlur}
       onKeyDown={handleKeyDown}
     />
