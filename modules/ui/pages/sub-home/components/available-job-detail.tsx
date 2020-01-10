68b44151838ae0a840ffdcca19377b5bcb8794c6
Meredith Lind <meredith.lind@atomicobject.com>
2020-01-10 14:06:36 -0500

    wrap available job details in mobile

--- modules/ui/pages/sub-home/components/available-job-detail.tsx
+++ modules/ui/pages/sub-home/components/available-job-detail.tsx
@@ -1,7 +1,9 @@
-import { makeStyles, Typography } from "@material-ui/core";
-import { formatIsoDateIfPossible } from "helpers/date";
+import { useIsMobile } from "hooks";
 import * as React from "react";
-import { DetailDayPartDisplay } from "ui/components/substitutes/detail-day-part-display";
+import {
+  AvailableJobDetailUI,
+  MobileAvailableJobDetailUI,
+} from "./available-job-details-ui";
 
 type Props = {
   locationName: string;
@@ -12,72 +14,11 @@ type Props = {
 };
 
 export const AvailableJobDetail: React.FC<Props> = props => {
-  const classes = useStyles();
+  const isMobile = useIsMobile();
 
-  return (
-    <div
-      className={[
-        classes.container,
-        props.shadeRow ? classes.shadedRow : classes.nonShadedRow,
-      ].join(" ")}
-    >
-      <div className={classes.date}>
-        <Typography className={classes.lightText}>
-          {formatIsoDateIfPossible(props.startTimeLocal, "EEE, MMM d")}
-        </Typography>
-      </div>
-      <div className={classes.location}>
-        <Typography className={classes.lightText}>
-          {props.locationName}
-        </Typography>
-      </div>
-
-      <DetailDayPartDisplay
-        dayPortion={props.dayPortion}
-        endTimeLocal={props.endTimeLocal}
-        startTimeLocal={props.startTimeLocal}
-        iconClassName={classes.dayPartIcon}
-        className={classes.dayPortion}
-      />
-    </div>
+  return isMobile ? (
+    <MobileAvailableJobDetailUI {...props} />
+  ) : (
+    <AvailableJobDetailUI {...props} />
   );
 };
-
-const useStyles = makeStyles(theme => ({
-  lightText: {
-    fontSize: theme.typography.pxToRem(14),
-    color: theme.customColors.edluminSubText,
-  },
-  nonShadedRow: {
-    background: theme.customColors.white,
-    padding: theme.spacing(1),
-  },
-  shadedRow: {
-    background: theme.customColors.lightGray,
-    padding: theme.spacing(1),
-  },
-
-  dayPartIcon: {
-    marginRight: theme.spacing(1),
-    marginLeft: theme.spacing(1),
-  },
-  container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-  },
-
-  date: {
-    flex: 1.7,
-  },
-  location: {
-    flex: 7,
-  },
-  dayPortion: {
-    display: "flex",
-    alignItems: "center",
-    flex: 7,
-  },
-}));
