940953f9b0393f336ed84545bcc56d70fd3f9ec5
Meredith Lind <meredith.lind@atomicobject.com>
2020-01-10 21:17:48 -0500

    pull out reuseable parts of assignment rows and hide actions for single job view

--- modules/ui/components/substitutes/assignment-row/assignment-row-ui.tsx
+++ modules/ui/components/substitutes/assignment-row/assignment-row-ui.tsx
@@ -1,12 +1,9 @@
 import { Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import * as DateFns from "date-fns";
-import { formatIsoDateIfPossible } from "helpers/date";
+import { useIsMobile } from "hooks";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { DayIcon } from "ui/components/day-icon";
-import { parseDayPortion } from "ui/components/helpers";
-import { useIsMobile } from "hooks";
+import { AssignmentDetailsUI } from "ui/components/substitutes/assignment-details/assignment-details-ui";
 
 type Props = {
   startDate: string;
@@ -40,23 +37,6 @@ export const AssignmentRowUI: React.FC<Props> = props => {
   const isMobile = useIsMobile();
   const { t } = useTranslation();
 
-  const startDate = DateFns.parseISO(props.startDate);
-  const endDate = DateFns.parseISO(props.endDate);
-  let vacancyDates = DateFns.format(startDate, "MMM d");
-  let vacancyDaysOfWeek = DateFns.format(startDate, "EEEE");
-
-  if (!DateFns.isEqual(startDate, endDate)) {
-    vacancyDaysOfWeek = `${DateFns.format(startDate, "EEE")}-${DateFns.format(
-      endDate,
-      "EEE"
-    )}`;
-    if (startDate.getMonth() === endDate.getMonth()) {
-      vacancyDates = `${vacancyDates}-${DateFns.format(endDate, "d")}`;
-    } else {
-      vacancyDates = `${vacancyDates}-${DateFns.format(endDate, "MMM d")}`;
-    }
-  }
-
   return (
     <div
       className={[
@@ -65,45 +45,8 @@ export const AssignmentRowUI: React.FC<Props> = props => {
         props.className,
       ].join(" ")}
     >
-      <div className={classes.dateContainer}>
-        <Typography className={classes.date} noWrap>
-          {vacancyDates}
-        </Typography>
-        <Typography className={classes.subText}>{vacancyDaysOfWeek}</Typography>
-      </div>
-      <div className={classes.location}>
-        <Typography className={classes.text}>{props.locationName}</Typography>
-        {props.organizationName && (
-          <Typography className={classes.subText}>
-            {props.organizationName}
-          </Typography>
-        )}
-      </div>
-      <div className={classes.position}>
-        <Typography className={classes.bold}>{props.positionName}</Typography>
-        <Typography className={classes.subText}>
-          {t("for")} {props.employeeName}
-        </Typography>
-      </div>
+      <AssignmentDetailsUI {...props} locationNames={[props.locationName]} />
 
-      <div className={classes.dayPartContainer}>
-        <DayIcon dayPortion={props.dayPortion} startTime={props.startTime} />
-
-        <div className={classes.dayPart}>
-          <Typography variant="h6" noWrap>{`${Math.round(
-            props.dayPortion
-          )} ${parseDayPortion(t, props.dayPortion)}`}</Typography>
-
-          <Typography className={classes.subText} noWrap>
-            {props.multipleTimes
-              ? t("Various")
-              : `${formatIsoDateIfPossible(
-                  props.startTime,
-                  "h:mm aaa"
-                )} - ${formatIsoDateIfPossible(props.endTime, "h:mm aaa")}`}
-          </Typography>
-        </div>
-      </div>
       {!props.forSpecificAssignment && (
         <div className={classes.confNumber}>
           <Typography className={classes.bold} noWrap>
@@ -139,33 +82,9 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     alignItems: "flex-start",
   },
-  dateContainer: {
-    flex: 4,
-    padding: `0 ${theme.typography.pxToRem(4)}`,
-  },
-  date: {
-    fontSize: theme.typography.pxToRem(18),
-    fontWeight: 500,
-  },
-  location: { flex: 11, padding: `0 ${theme.typography.pxToRem(4)}` },
-  position: { flex: 7, padding: `0 ${theme.typography.pxToRem(4)}` },
-  subText: {
-    color: theme.customColors.edluminSubText,
-  },
+
   bold: {
     fontWeight: 500,
-  },
-  text: {
-    fontSize: theme.typography.pxToRem(18),
-  },
-  dayPartContainer: {
-    display: "flex",
-    flex: 8,
-    padding: `0 ${theme.typography.pxToRem(4)}`,
-  },
-  dayPart: {
-    display: "inline-block",
-    paddingLeft: theme.spacing(1),
   },
   confNumber: { flex: 4, padding: `0 ${theme.typography.pxToRem(4)}` },
   cancel: { color: theme.customColors.darkRed },
--- modules/ui/pages/sub-home/components/available-job.tsx
+++ modules/ui/pages/sub-home/components/available-job.tsx
@@ -4,21 +4,15 @@ import {
   IconButton,
   makeStyles,
   Popper,
-  Typography,
 } from "@material-ui/core";
 import { ReceiptOutlined } from "@material-ui/icons";
-import { useIsMobile } from "hooks";
-import format from "date-fns/format";
-import isEqual from "date-fns/isEqual";
-import parseISO from "date-fns/parseISO";
 import { Vacancy } from "graphql/server-types.gen";
-import { formatIsoDateIfPossible } from "helpers/date";
+import { useIsMobile } from "hooks";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { parseDayPortion } from "ui/components/helpers";
-import { AvailableJobDetail } from "./available-job-detail";
-import { DayIcon } from "ui/components/day-icon";
 import { ExpandOrCollapseIndicator } from "ui/components/substitutes/expand-or-collapse-indicator";
+import { AvailableJobDetail } from "./available-job-detail";
+import { AssignmentDetails } from "ui/components/substitutes/assignment-details";
 
 type Props = {
   vacancy: Pick<
@@ -55,30 +49,6 @@ export const AvailableJob: React.FC<Props> = props => {
   const hasDetails = vacancy.details!.length > 1;
   const acceptButtonDisabled = hasDetails && !expanded;
   const showDetails = (expanded || props.forSingleJob) && hasDetails;
-
-  const startDate = parseISO(vacancy.startDate);
-  const endDate = parseISO(vacancy.endDate);
-  let vacancyDates = format(startDate, "MMM d");
-  let vacancyDaysOfWeek = format(startDate, "EEEE");
-  if (!isEqual(startDate, endDate)) {
-    vacancyDaysOfWeek = `${format(startDate, "EEE")} - ${format(
-      endDate,
-      "EEE"
-    )}`;
-    if (startDate.getMonth() === endDate.getMonth()) {
-      vacancyDates = `${vacancyDates} - ${format(endDate, "d")}`;
-    } else {
-      vacancyDates = `${vacancyDates} - ${format(endDate, "MMM d")}`;
-    }
-  }
-
-  const locationNames = [
-    ...new Set(vacancy.details!.map(d => d!.location!.name)),
-  ];
-  const locationNameText =
-    locationNames.length > 1
-      ? `${locationNames[0]} +${locationNames.length - 1}`
-      : locationNames[0];
 
   const handleShowNotes = (event: React.MouseEvent<HTMLElement>) => {
     event.stopPropagation();
@@ -124,84 +94,38 @@ export const AvailableJob: React.FC<Props> = props => {
       >
         <div className={classes.container}>
           <div className={classes.infoContainer}>
-            <div className={classes.date}>
-              {isMobile ? (
-                <Typography variant="h6">{`${vacancyDates}, ${vacancyDaysOfWeek}`}</Typography>
-              ) : (
-                <>
-                  <Typography variant="h6">{vacancyDates}</Typography>
-                  <Typography className={classes.lightText}>
-                    {vacancyDaysOfWeek}
-                  </Typography>
-                </>
-              )}
-            </div>
-
-            <div className={classes.location}>
-              <Typography className={classes.text}>
-                {locationNameText ?? t("Unknown")}
-              </Typography>
-              <Typography className={classes.lightText}>
-                {vacancy.organization.name}
-              </Typography>
-            </div>
-
-            <div className={classes.position}>
-              <Typography className={classes.text}>
-                {vacancy.position!.name}
-              </Typography>
-              <Typography className={classes.lightText}>{`for ${
-                vacancy.absence!.employee!.firstName
-              } ${vacancy.absence!.employee!.lastName}`}</Typography>
-            </div>
-
-            <div className={classes.dayPartContainer}>
-              <DayIcon
-                dayPortion={vacancy.totalDayPortion}
-                startTime={vacancy.startTimeLocal}
-              />
-              <div className={classes.dayPart}>
-                <Typography className={classes.text}>{`${Math.round(
-                  vacancy.totalDayPortion
-                )} ${parseDayPortion(t, vacancy.totalDayPortion)}`}</Typography>
-                <Typography
-                  className={classes.lightText}
-                >{`${formatIsoDateIfPossible(
-                  vacancy.startTimeLocal,
-                  "h:mm aaa"
-                )} - ${formatIsoDateIfPossible(
-                  vacancy.endTimeLocal,
-                  "h:mm aaa"
-                )}`}</Typography>
-              </div>
-            </div>
+            <AssignmentDetails vacancy={vacancy} />
           </div>
 
           <div className={classes.actionContainer}>
-            <div className={classes.notes}>
-              {vacancy.notesToReplacement &&
-                renderNotesPopper(vacancy.notesToReplacement)}
-            </div>
+            {!props.forSingleJob && (
+              <>
+                <div className={classes.notes}>
+                  {vacancy.notesToReplacement &&
+                    renderNotesPopper(vacancy.notesToReplacement)}
+                </div>
 
-            <div className={classes.actionItem}>
-              <Button
-                onClick={() => handleDismiss()}
-                className={classes.lightUnderlineText}
-              >
-                {t("Dismiss")}
-              </Button>
-            </div>
-            <div className={classes.actionItem}>
-              <Button
-                variant="outlined"
-                disabled={acceptButtonDisabled}
-                onClick={() =>
-                  props.onAccept(vacancy.organization.id, vacancy.id)
-                }
-              >
-                {t("Accept")}
-              </Button>
-            </div>
+                <div className={classes.actionItem}>
+                  <Button
+                    onClick={() => handleDismiss()}
+                    className={classes.lightUnderlineText}
+                  >
+                    {t("Dismiss")}
+                  </Button>
+                </div>
+                <div className={classes.actionItem}>
+                  <Button
+                    variant="outlined"
+                    disabled={acceptButtonDisabled}
+                    onClick={() =>
+                      props.onAccept(vacancy.organization.id, vacancy.id)
+                    }
+                  >
+                    {t("Accept")}
+                  </Button>
+                </div>
+              </>
+            )}
           </div>
         </div>
 
@@ -239,13 +163,6 @@ export const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
   },
 
-  text: {
-    fontSize: theme.typography.pxToRem(14),
-  },
-  lightText: {
-    fontSize: theme.typography.pxToRem(14),
-    color: theme.customColors.edluminSubText,
-  },
   lightUnderlineText: {
     fontSize: theme.typography.pxToRem(14),
     color: theme.customColors.edluminSubText,
@@ -277,33 +194,18 @@ export const useStyles = makeStyles(theme => ({
 
   actionContainer: (props: StyleProps) => ({
     display: "flex",
-    justifyContent: "space-around",
+    justifyContent: props.isMobile ? "space-between" : "space-around",
     alignItems: "center",
     flex: 1,
-    flexDirection: props.isMobile ? ("column" as "column") : ("row" as "row"),
+    flexDirection: props.isMobile
+      ? ("column-reverse" as "column-reverse")
+      : ("row" as "row"),
   }),
   rowContainer: {
     display: "flex",
     flexDirection: "column",
   },
 
-  date: {
-    flex: 4,
-    padding: `0 ${theme.typography.pxToRem(4)}`,
-  },
-  location: { flex: 11, padding: `0 ${theme.typography.pxToRem(4)}` },
-  position: { flex: 7, padding: `0 ${theme.typography.pxToRem(4)}` },
-  subText: {
-    color: theme.customColors.edluminSubText,
-  },
-  dayPartContainer: {
-    display: "flex",
-    flex: 8,
-    padding: `0 ${theme.typography.pxToRem(4)}`,
-  },
-  dayPart: {
-    paddingLeft: theme.spacing(),
-  },
   actionItem: {
     flex: 2,
   },
