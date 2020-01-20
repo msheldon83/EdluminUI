7eb3161e8e0ae843cba9fa73c095914938c790b8
David Nawn <dnawn@edustaff.org>
2020-01-20 16:43:59 -0500

    Add/Remove attributes. Still have to provide a way to update expiration date and show the endAdornment icon on the DatePicker component.

--- modules/ui/pages/sub-positions-attributes/index.tsx
+++ modules/ui/pages/sub-positions-attributes/index.tsx
@@ -1,19 +1,24 @@
 import { makeStyles } from "@material-ui/styles";
 import { useIsMobile } from "hooks";
 import * as React from "react";
-import { useQueryBundle } from "graphql/hooks";
+import { useQueryBundle, useMutationBundle } from "graphql/hooks";
 import { useTranslation } from "react-i18next";
 import { Typography, Grid, Link } from "@material-ui/core";
 import { useHistory } from "react-router";
 import { useRouteParams } from "ui/routes/definition";
 import { PersonViewRoute } from "ui/routes/people";
 import { PageTitle } from "ui/components/page-title";
-import { useEndorsements } from "reference-data/endorsements";
-import { GetAllPositionTypesWithReplacementCriteria } from "./graphql/get-all-position-types.gen";
-import { compact } from "lodash-es";
-import { useMemo, useState } from "react";
 import { GetSubstituteById } from "../people/graphql/substitute/get-substitute-by-id.gen";
-import { Section } from "ui/components/section";
+import {
+  SubPositionTypesAndAttributesEdit,
+  Attributes,
+} from "./components/position-types-attributes";
+import { useMemo, useCallback } from "react";
+import { ShowErrors } from "ui/components/error-helpers";
+import { AddEmployeeEndorsement } from "ui/components/employee/graphql/add-endorsement.gen";
+import { RemoveEmployeeEndorsement } from "ui/components/employee/graphql/remove-endorsement.gen";
+import { UpdateEmployeeEndorsement } from "ui/components/employee/graphql/update-endorsement.gen";
+import { useSnackbar } from "hooks/use-snackbar";
 
 type Props = {};
 
@@ -23,34 +28,11 @@ export const SubPositionsAttributes: React.FC<Props> = props => {
   const classes = useStyles();
   const isMobile = useIsMobile();
   const params = useRouteParams(PersonViewRoute);
-  const orgId = params.organizationId;
-  const orgUserId = params.orgUserId;
-  const [endorsementSearchText, setEndorsementSearchText] = useState<
-    string | undefined
-  >(undefined);
-  const allEndorsements = useEndorsements(orgId, false, endorsementSearchText);
-
-  // Get all of the Position Types in the Org
-  const positionTypeResponse = useQueryBundle(
-    GetAllPositionTypesWithReplacementCriteria,
-    {
-      fetchPolicy: "cache-first",
-      variables: { orgId },
-    }
-  );
-  const allPositionTypes = useMemo(() => {
-    if (
-      positionTypeResponse.state === "DONE" &&
-      positionTypeResponse.data.positionType
-    ) {
-      return compact(positionTypeResponse.data.positionType.all) ?? [];
-    }
-    return [];
-  }, [positionTypeResponse]);
+  const { openSnackbar } = useSnackbar();
 
   // Get the Substitute info
   const getSubstituteById = useQueryBundle(GetSubstituteById, {
-    variables: { id: orgUserId },
+    variables: { id: params.orgUserId },
   });
 
   const substitute =
@@ -58,26 +40,91 @@ export const SubPositionsAttributes: React.FC<Props> = props => {
       ? undefined
       : getSubstituteById?.data?.orgUser?.byId?.substitute;
 
-  // Determine which Position Types the Sub isn't currently qualified for
-  const qualifiedPositionTypes = useMemo(
-    () => substitute?.qualifiedPositionTypes ?? [],
-    [substitute]
-  );
-  const unqualifiedPositionTypes = useMemo(() => {
-    return allPositionTypes.filter(
-      p => !qualifiedPositionTypes.find(q => q?.id === p.id)
-    );
-  }, [qualifiedPositionTypes, allPositionTypes]);
+  const currentAttributes: Attributes[] = useMemo(() => {
+    if (!substitute?.attributes) {
+      return [];
+    }
 
-  // Determine which Endorsements the Sub doesn't already have
-  const currentEndorsements = useMemo(() => substitute?.attributes ?? [], [
-    substitute,
-  ]);
-  const missingEndorsements = useMemo(() => {
-    return allEndorsements.filter(
-      e => !currentEndorsements.find(c => c?.endorsement?.id === e.id)
-    );
-  }, [currentEndorsements, allEndorsements]);
+    return substitute.attributes.map(a => {
+      return {
+        endorsementId: a.endorsement.id,
+        name: a.endorsement.name,
+        expirationDate: a.expirationDate,
+        expires: a.endorsement.expires,
+      };
+    });
+  }, [substitute]);
+
+  // Define mutations and functions
+  const [addEmployeeEndorsement] = useMutationBundle(AddEmployeeEndorsement, {
+    onError: error => {
+      ShowErrors(error, openSnackbar);
+    },
+  });
+
+  const addAttribute = useCallback(
+    async (endorsementId: string) => {
+      const response = await addEmployeeEndorsement({
+        variables: {
+          employeeEndorsementInput: {
+            orgId: Number(params.organizationId),
+            employeeId: Number(params.orgUserId),
+            endorsementId: Number(endorsementId),
+          },
+        },
+      });
+      return !!response?.data;
+    },
+    [addEmployeeEndorsement]
+  );
+
+  const [updateEmployeeEndorsement] = useMutationBundle(
+    UpdateEmployeeEndorsement,
+    {
+      onError: error => {
+        ShowErrors(error, openSnackbar);
+      },
+    }
+  );
+
+  const updateAttribute = useCallback(
+    async (endorsementId: string, expirationDate?: Date | undefined) => {
+      const response = await updateEmployeeEndorsement({
+        variables: {
+          employeeEndorsementInput: {
+            orgId: Number(params.organizationId),
+            employeeId: Number(params.orgUserId),
+            endorsementId: Number(endorsementId),
+            expirationDate,
+          },
+        },
+      });
+      return !!response?.data;
+    },
+    [updateEmployeeEndorsement]
+  );
+
+  const [removeEmployeeEndorsement] = useMutationBundle(
+    RemoveEmployeeEndorsement,
+    {
+      onError: error => {
+        ShowErrors(error, openSnackbar);
+      },
+    }
+  );
+
+  const removeAttribute = useCallback(
+    async (endorsementId: string) => {
+      const response = await removeEmployeeEndorsement({
+        variables: {
+          employeeId: Number(params.orgUserId),
+          endorsementId: Number(endorsementId),
+        },
+      });
+      return !!response?.data;
+    },
+    [removeEmployeeEndorsement]
+  );
 
   if (getSubstituteById.state === "LOADING") {
     return <></>;
@@ -89,151 +136,17 @@ export const SubPositionsAttributes: React.FC<Props> = props => {
     <>
       <Typography variant="h5">{fullName}</Typography>
       <PageTitle title={t("Position types & Attributes")} />
-      <Section>
-        <Grid container spacing={4}>
-          <Grid item xs={6}>
-            <Typography variant="h5" className={classes.sectionHeader}>
-              {t("Qualified for position types")}
-            </Typography>
-            {qualifiedPositionTypes.map((p, i) => {
-              return (
-                <div key={i} className={getRowClasses(classes, i)}>
-                  {p!.name}
-                </div>
-              );
-            })}
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h5" className={classes.sectionHeader}>
-              {t("Not qualified for position types")}
-            </Typography>
-            {unqualifiedPositionTypes.length === 0 ? (
-              <div className={classes.allOrNoneRow}>{t("None")}</div>
-            ) : (
-              unqualifiedPositionTypes.map((p, i) => {
-                return (
-                  <div key={i} className={getRowClasses(classes, i)}>
-                    {p!.name}
-                  </div>
-                );
-              })
-            )}
-          </Grid>
-        </Grid>
-      </Section>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Section className={classes.attributesSection}>
-            <Typography variant="h5" className={classes.sectionHeader}>
-              {t("Selected attributes")}
-            </Typography>
-            <Grid container item xs={12}>
-              {currentEndorsements.length === 0 ? (
-                <div className={classes.allOrNoneRow}>{t("None")}</div>
-              ) : (
-                <>
-                  <Grid item xs={6}>
-                    {t("Name")}
-                  </Grid>
-                  <Grid item xs={6}>
-                    {t("Expires")}
-                  </Grid>
-                  {currentEndorsements.map((p, i) => {
-                    return (
-                      <div key={i} className={getRowClasses(classes, i)}>
-                        <Grid item xs={6}>
-                          {p!.endorsement!.name}
-                        </Grid>
-                        <Grid item xs={6}>
-                          {/* TODO: Textbox to edit and save on blur (or call prop function for both create/edit)  */}
-                          {p!.expirationDate}
-                        </Grid>
-                      </div>
-                    );
-                  })}
-                </>
-              )}
-            </Grid>
-          </Section>
-        </Grid>
-        <Grid item xs={6}>
-          <Section className={classes.attributesSection}>
-            <Typography variant="h5" className={classes.sectionHeader}>
-              {t("Available attributes")}
-            </Typography>
-            {/* TODO: Add search box for client side search */}
-            <Grid container item xs={12}>
-              {missingEndorsements.map((p, i) => {
-                return (
-                  <div
-                    key={i}
-                    className={`${
-                      classes.availableAttributeRow
-                    } ${getRowClasses(classes, i)}`}
-                  >
-                    <div>{p!.name}</div>
-                    <div>
-                      <Link className={classes.action} onClick={() => {}}>
-                        {t("Add")}
-                      </Link>
-                    </div>
-                  </div>
-                );
-              })}
-            </Grid>
-          </Section>
-        </Grid>
-      </Grid>
+      <SubPositionTypesAndAttributesEdit
+        organizationId={params.organizationId}
+        orgUserId={params.orgUserId}
+        qualifiedPositionTypes={substitute?.qualifiedPositionTypes ?? []}
+        currentAttributes={currentAttributes}
+        addAttribute={addAttribute}
+        updateAttribute={updateAttribute}
+        removeAttribute={removeAttribute}
+      />
     </>
   );
 };
 
-const useStyles = makeStyles(theme => ({
-  sectionHeader: {
-    marginBottom: theme.spacing(2),
-  },
-  allOrNoneRow: {
-    color: theme.customColors.edluminSubText,
-  },
-  row: {
-    width: "100%",
-    padding: theme.spacing(),
-  },
-  firstRow: {
-    borderTop: `${theme.typography.pxToRem(1)} solid ${
-      theme.customColors.medLightGray
-    }`,
-  },
-  shadedRow: {
-    background: theme.customColors.lightGray,
-    borderTop: `${theme.typography.pxToRem(1)} solid ${
-      theme.customColors.medLightGray
-    }`,
-    borderBottom: `${theme.typography.pxToRem(1)} solid ${
-      theme.customColors.medLightGray
-    }`,
-  },
-  availableAttributeRow: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  action: {
-    cursor: "pointer",
-  },
-  attributesSection: {
-    height: "100%",
-  },
-}));
-
-const getRowClasses = (classes: any, index: number): string => {
-  const rowClasses = [classes.row];
-  if (index === 0) {
-    rowClasses.push(classes.firstRow);
-  }
-  if (index % 2 === 1) {
-    rowClasses.push(classes.shadedRow);
-  }
-
-  return rowClasses.join(" ");
-};
+const useStyles = makeStyles(theme => ({}));
