11a8d7245a5809aabbeec39cda13778d30f8ae09
Meredith Lind <meredith.lind@atomicobject.com>
2020-01-21 13:16:26 -0500

    add tab content and mutation

--- modules/ui/pages/absence-reason/add.tsx
+++ modules/ui/pages/absence-reason/add.tsx
@@ -1,31 +1,140 @@
-import { makeStyles, useTheme } from "@material-ui/styles";
-import { useIsMobile } from "hooks";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { useMutationBundle } from "graphql/hooks";
+import {
+  AbsenceReasonTrackingTypeId,
+  AssignmentType,
+} from "graphql/server-types.gen";
 import * as React from "react";
+import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router";
 import { PageTitle } from "ui/components/page-title";
+import { Step, TabbedHeader } from "ui/components/tabbed-header";
 import {
-  AbsenceReasonRoute,
   AbsenceReasonAddRoute,
+  AbsenceReasonRoute,
+  AbsenceReasonViewEditRoute,
 } from "ui/routes/absence-reason";
 import { useRouteParams } from "ui/routes/definition";
-import { Button, Typography } from "@material-ui/core";
+import { AbsenceReasonSettings } from "./absence-reason-settings";
+import { AddBasicInfo } from "./add-basic-info";
+import { CreateAbsenceReason } from "./graphql/create-absence-reason.gen";
 
 type Props = {};
 
 export const AbsenceReasonAddPage: React.FC<Props> = props => {
   const { t } = useTranslation();
   const history = useHistory();
-  const theme = useTheme();
   const classes = useStyles();
-  const isMobile = useIsMobile();
   const params = useRouteParams(AbsenceReasonAddRoute);
 
-  const [triggerError, setTriggerError] = React.useState(false);
+  const namePlaceholder = t("Professional Development");
+  const [basicInfo, setBasicInfo] = React.useState<{
+    name: string;
+    externalId?: string;
+  } | null>(null);
+  const [createAbsenceReason] = useMutationBundle(CreateAbsenceReason);
 
+  const settingsOnSubmit = useCallback(
+    async (updatedValues: {
+      allowNegativeBalance: boolean;
+      isBucket: boolean;
+      description?: string;
+      absenceReasonTrackingTypeId?: AbsenceReasonTrackingTypeId;
+      appliesToAssignmentTypes?: AssignmentType;
+    }) => {
+      if (!basicInfo) {
+        return;
+      }
+      const {
+        allowNegativeBalance,
+        isBucket,
+        absenceReasonTrackingTypeId: absenceReasonTrackingId,
+        appliesToAssignmentTypes,
+        description,
+      } = updatedValues;
+
+      const result = await createAbsenceReason({
+        variables: {
+          absenceReason: {
+            orgId: Number(params.organizationId),
+            ...basicInfo,
+            allowNegativeBalance,
+            isBucket,
+            absenceReasonTrackingId,
+            appliesToAssignmentTypes,
+            description,
+          },
+        },
+      });
+      const id = result.data?.orgRef_AbsenceReason?.create?.id;
+      if (id) {
+        history.push(
+          AbsenceReasonViewEditRoute.generate({
+            ...params,
+            absenceReasonId: id,
+          })
+        );
+      }
+    },
+    [createAbsenceReason, basicInfo, params, history]
+  );
+
+  const [triggerError, setTriggerError] = React.useState(false);
   if (triggerError) {
     throw Error("error!");
   }
+
+  const steps: Array<Step> = [
+    {
+      stepNumber: 0,
+      name: t("Basic Info"),
+      content: (setStep: React.Dispatch<React.SetStateAction<number>>) => {
+        return (
+          <AddBasicInfo
+            namePlaceholder={namePlaceholder}
+            name={basicInfo?.name ?? ""}
+            externalId={basicInfo?.externalId}
+            onNameChange={name => {
+              setBasicInfo({
+                name,
+              });
+            }}
+            onSubmit={(name, externalId) => {
+              setBasicInfo({
+                name,
+                externalId,
+              });
+              setStep(steps[1].stepNumber);
+            }}
+            onCancel={() => {
+              history.push(AbsenceReasonRoute.generate(params));
+            }}
+          />
+        );
+      },
+    },
+    {
+      stepNumber: 1,
+      name: t("Settings"),
+      content: () => {
+        return (
+          <AbsenceReasonSettings
+            allowNegativeBalance={false}
+            isBucket={false}
+            absenceReasonTrackingTypeId={AbsenceReasonTrackingTypeId.Hourly}
+            appliesToAssignmentTypes={AssignmentType.ContractAssignment}
+            description={""}
+            onSubmit={settingsOnSubmit}
+            onCancel={() => {
+              history.push(AbsenceReasonRoute.generate(params));
+            }}
+          />
+        );
+      },
+    },
+  ];
 
   return (
     <>
@@ -33,12 +142,11 @@ export const AbsenceReasonAddPage: React.FC<Props> = props => {
         <PageTitle title={t("Create new absence reason")} />
         <Typography variant="h1">
           {name || (
-            <span className={classes.placeholder}>
-              {t("Professional Development")}
-            </span>
+            <span className={classes.placeholder}>{namePlaceholder}</span>
           )}
         </Typography>
       </div>
+      <TabbedHeader steps={steps} isWizard={true} showStepNumber={true} />
     </>
   );
 };
