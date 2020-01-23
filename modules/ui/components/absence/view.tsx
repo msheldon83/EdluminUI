10432b5d10902176e4815692bae42a6c7bdc1ec1
David Nawn <dnawn@edustaff.org>
2020-01-23 11:49:53 -0500

    Fix a conditional hook rendering issue

--- modules/ui/components/absence/view.tsx
+++ modules/ui/components/absence/view.tsx
@@ -45,7 +45,7 @@ export const View: React.FC<Props> = props => {
   const { openSnackbar } = useSnackbar();
   const absenceReasons = useAbsenceReasons(props.orgId);
 
-  const absence = props.absence;
+  const absence = useMemo(() => props.absence, [props.absence]);
   const absenceStartDate = useMemo(() => new Date(absence.startDate), [
     absence.startDate,
   ]);
@@ -92,6 +92,25 @@ export const View: React.FC<Props> = props => {
     },
   });
 
+  // In order to appropriately show the Absence times alongside the Sub Vacancy Details
+  // we have to make sure each Vacancy has a populated Absence property to match
+  // against in <VacancyDetails />. Instead of having the server return the Absence, and
+  // then the Vacancies and then the Absence again on each Vacancy, we can just manifest
+  // that information here and pass into <VacancyDetails />.
+  const vacancies = useMemo(() => {
+    if (!absence) {
+      return [];
+    }
+
+    const a = absence;
+    return (absence.vacancies?.map(v => {
+      return {
+        ...v,
+        absence: a,
+      };
+    }) ?? []) as Vacancy[];
+  }, [absence]);
+
   if (!absence) {
     return null;
   }
@@ -132,21 +151,6 @@ export const View: React.FC<Props> = props => {
       buttonProps: { className: classes.absenceDate },
     };
   });
-
-  // In order to appropriately show the Absence times alongside the Sub Vacancy Details
-  // we have to make sure each Vacancy has a populated Absence property to match
-  // against in <VacancyDetails />. Instead of having the server return the Absence, and
-  // then the Vacancies and then the Absence again on each Vacancy, we can just manifest
-  // that information here and pass into <VacancyDetails />.
-  const vacancies = useMemo(() => {
-    const a = props.absence;
-    return (props.absence.vacancies?.map(v => {
-      return {
-        ...v,
-        absence: a,
-      };
-    }) ?? []) as Vacancy[];
-  }, [props.absence]);
 
   return (
     <div>
