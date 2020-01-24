afc168fd901d51be5a1624bfbc26ea6267261d9b
David Nawn <dnawn@edustaff.org>
2020-01-24 10:00:32 -0500

    Logic change on the backend necessitated an update here to match

--- modules/ui/pages/create-absence/assign-sub/icons/visible-icon.tsx
+++ modules/ui/pages/create-absence/assign-sub/icons/visible-icon.tsx
@@ -14,27 +14,24 @@ export const VisibleIcon: React.FC<Props> = props => {
   const classes = useStyles();
   const { t } = useTranslation();
 
-  if (!props.isAvailableToSubWhenSearching) {
-    return <VisibilityOff className={classes.icon} />;
-  }
-
-  if (props.visible) {
+  if (props.isAvailableToSubWhenSearching) {
+    // Sub can currently see this job
     return <Visibility className={classes.icon} />;
   }
 
-  // This shouldn't happen, but just in case and to make TS happy
-  // with the use of "parseISO(props.visibleOn)" below
-  if (!props.visibleOn) {
-    return <VisibilityOff className={classes.icon} />;
+  if (props.visibleOn) {
+    // Sub will eventually be able to see this job
+    const iso = parseISO(props.visibleOn);
+    const date = isValid(iso) && format(iso, "MMM d, yyyy h:mm aa");
+    return (
+      <Tooltip title={`${t("Visible on")} ${date}`}>
+        <img src={require("ui/icons/visibility_time.svg")} />
+      </Tooltip>
+    );
   }
 
-  const iso = parseISO(props.visibleOn);
-  const date = isValid(iso) && format(iso, "MMM d, yyyy h:mm aa");
-  return (
-    <Tooltip title={`${t("Visible on")} ${date}`}>
-      <img src={require("ui/icons/visibility_time.svg")} />
-    </Tooltip>
-  );
+  // Sub will never be able to see this job
+  return <VisibilityOff className={classes.icon} />;
 };
 
 const useStyles = makeStyles(theme => ({
