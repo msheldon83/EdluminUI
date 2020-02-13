d19cc8e75543c23055c4720c2d320d6ceefc06f5
Chris Avenick <chris@stillrocksolutions.com>
2020-02-13 12:24:48 -0500

    removed some commented out code

--- modules/ui/components/routing/red-rover-prompt.tsx
+++ modules/ui/components/routing/red-rover-prompt.tsx
@@ -1,22 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Typography,
-  Divider,
-} from "@material-ui/core";
-//import { makeStyles } from "@material-ui/styles";
+import { Dialog, DialogActions, Divider } from "@material-ui/core";
 import { ButtonDisableOnClick } from "ui/components/button-disable-on-click";
-import { TextButton } from "ui/components/text-button";
-//import { useTranslation } from "react-i18next";
 
 const RedRoverPrompt = (message: any, callback: any) => {
-  //const classes = useStyles();
-  //const { t } = useTranslation();
-
   const container = document.createElement("div");
   container.setAttribute("custom-confirmation-navigation", "");
   document.body.appendChild(container);
@@ -26,12 +13,6 @@ const RedRoverPrompt = (message: any, callback: any) => {
   };
   ReactDOM.render(
     <Dialog open={true} onClose={() => closeModal(false)}>
-      {/*<DialogTitle disableTypography>
-        <Typography variant="h5">{"This page has unsaved changes"}</Typography>
-      </DialogTitle>
-      <DialogContent>
-        <Typography>{message}</Typography>
-      </DialogContent>*/}
       <div style={{ flex: "0 0 auto", margin: 0, padding: "16px 24px" }}>
         <h5
           style={{
@@ -80,14 +61,3 @@ const RedRoverPrompt = (message: any, callback: any) => {
   );
 };
 export default RedRoverPrompt;
-
-/*const useStyles = makeStyles(theme => ({
-  buttonSpacing: {
-    paddingRight: theme.spacing(2),
-  },
-  divider: {
-    color: theme.customColors.gray,
-    marginTop: theme.spacing(2),
-  },
-  cancel: { color: theme.customColors.darkRed },
-}));*/
