2293f4e84ebe116bc609cac5003182951031ca9d
David Nawn <dnawn@edustaff.org>
2020-02-17 12:07:28 -0500

    Handle a race condition with the Zendesk snippet script loading

--- modules/ui/app-chrome/help-widget.tsx
+++ modules/ui/app-chrome/help-widget.tsx
@@ -1,25 +1,46 @@
 import * as React from "react";
 import { useMyUserAccess } from "reference-data/my-user-access";
 
-declare let $zopim: any;
-declare let zE: any;
 export const HelpWidget: React.FC<{}> = props => {
   const userAccess = useMyUserAccess();
   const user = userAccess?.me?.user;
   const showChat = user?.orgUsers?.some(x => x?.administrator?.isSuperUser);
 
-  if (showChat) {
-    zE("webWidget", "identify", {
-      name: `${user?.firstName} ${user?.lastName}`,
-      email: user?.loginEmail,
-    });
-    if ($zopim) {
-      $zopim.livechat.button.show();
-    }
-  } else {
-    if ($zopim) {
-      $zopim.livechat.button.hide();
-    }
+  try {
+    const loopsToWait = 10;
+    let currentLoopCount = 0;
+    const waitForZopim = setInterval(function() {
+      if (currentLoopCount >= loopsToWait) {
+        clearInterval(waitForZopim);
+        return;
+      }
+
+      if (
+        (window as any).$zopim === undefined ||
+        (window as any).$zopim.livechat === undefined
+      ) {
+        currentLoopCount = currentLoopCount + 1;
+        return;
+      }
+
+      const zopim = (window as any).$zopim;
+      if (showChat) {
+        zopim(function() {
+          zopim.livechat.setName(`${user?.firstName} ${user?.lastName}`);
+          zopim.livechat.setEmail(user?.loginEmail);
+        });
+        zopim.livechat.button.show();
+      } else {
+        zopim.livechat.button.hide();
+      }
+
+      clearInterval(waitForZopim);
+    }, 100);
+  } catch (e) {
+    // Not loading chat should not affect the User
+    // using the rest of the application
+    console.error(e);
   }
+
   return <></>;
 };
