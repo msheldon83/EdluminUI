4f593224029464da1fdf5f6d9df3e505142dc9b6
chrisavenick <56037646+chrisavenick@users.noreply.github.com>
2020-02-19 17:45:08 -0500

    Update index.tsx

--- modules/ui/pages/employee-sub-preferences/index.tsx
+++ modules/ui/pages/employee-sub-preferences/index.tsx
@@ -107,6 +107,7 @@ export const EmployeeSubstitutePreferencePage: React.FC<{}> = props => {
         removeFavoritePermission={[PermissionEnum.EmployeeSaveFavoriteSubs]}
         addToBlockedPermission={[PermissionEnum.EmployeeSaveBlockedSubs]}
         addToFavoritePermission={[PermissionEnum.EmployeeSaveFavoriteSubs]}
+        isLocationOnly={false}
       ></SubstitutePreferences>
     </>
   );
