c6408fbf4b801b8fd72d43da1ba8180eecc548df
Chris Avenick <chris@stillrocksolutions.com>
2020-03-16 09:15:58 -0400

    sub available jobs

--- modules/ui/components/substitutes/assignment-details/index.tsx
+++ modules/ui/components/substitutes/assignment-details/index.tsx
@@ -3,11 +3,16 @@ import { useMemo } from "react";
 import { AssignmentDetailsUI } from "./assignment-details-ui";
 import { compact, groupBy } from "lodash-es";
 import { formatIsoDateIfPossible } from "helpers/date";
+import { useTranslation } from "react-i18next";
 
 type Detail = {
   startTimeLocal?: string;
   endTimeLocal?: string;
   location?: {
+    name: string | null;
+  } | null;
+  vacancyReason?: {
+    id: string | null;
     name: string | null;
   } | null;
 } | null;
@@ -41,10 +46,14 @@ type Props = {
 
 export const AssignmentDetails: React.FC<Props> = props => {
   const { vacancy } = props;
+  const { t } = useTranslation();
 
-  const employeeName = `${vacancy.absence!.employee!.firstName} ${
-    vacancy.absence!.employee!.lastName
-  }`;
+  const employeeName =
+    vacancy.details && vacancy.details[0]?.vacancyReason
+      ? `${t("Vacancy")}: ${vacancy.position?.title}`
+      : `${vacancy.absence!.employee!.firstName} ${
+          vacancy.absence!.employee!.lastName
+        }`;
 
   const locationNames = useMemo(
     () => compact(vacancy.details!.map(d => d!.location!.name)),
