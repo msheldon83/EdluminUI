8c1414174cc2b8a42d03504eaf935cace3dffc7b
cwstra <cwstra@gmail.com>
2020-03-17 01:14:57 -0400

    Added Selected date dummy class

--- modules/ui/components/absence/create-absence-calendar.tsx
+++ modules/ui/components/absence/create-absence-calendar.tsx
@@ -59,10 +59,10 @@ export const CreateAbsenceCalendar: React.FC<Props> = props => {
       selectedAbsenceDates.map(date => {
         return {
           date,
-          buttonProps: { className: classes.selectedAbsenceDate },
+          buttonProps: { className: classes.selectedAbsenceDate+" selectedAbsenceDate" },
         };
       }),
-    [selectedAbsenceDates, classes.selectedAbsenceDate]
+    [selectedAbsenceDates, classes.selectedAbsenceDate+" selectedAbsenceDate"]
   );
 
   const customDates = useMemo(
