e178b0855464034f319287a8eb4c769fe8465b7f
Allen Dzedzy <adzedzy@edustaff.org>
2020-03-31 11:17:53 -0400

    remove old import

--- modules/ui/pages/profile/index.tsx
+++ modules/ui/pages/profile/index.tsx
@@ -11,7 +11,6 @@ import { useMyUserAccess } from "reference-data/my-user-access";
 import { useTranslation } from "react-i18next";
 import { GetUserById } from "ui/pages/users/graphql/get-user-by-id.gen";
 import { VerifyPhoneNumber } from "ui/pages/profile/graphql/verify-phone-number.gen";
-import { AlertBox } from "ui/components/alert";
 import { useIsImpersonating } from "reference-data/is-impersonating";
 import { useHistory } from "react-router";
 type Props = {};
