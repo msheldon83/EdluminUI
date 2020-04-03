a162ea560ee1cc86900d67aa526e81c30f287d2c
Allen Dzedzy <adzedzy@edustaff.org>
2020-04-03 14:29:16 -0400

    stash

--- modules/ui/pages/organizations/org-contact-info.tsx
+++ modules/ui/pages/organizations/org-contact-info.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
+import { useQueryBundle } from "graphql/hooks";
+import { AppChromeRoute } from "ui/routes/app-chrome";
+import { useRouteParams } from "ui/routes/definition";
+import { makeStyles } from "@material-ui/styles";
+import { GetOrganizationContactsById } from "ui/pages/organizations/graphql/get-organization-contacts.gen";
 
 export const OrganicationContactInfo: React.FC<{}> = props => {
-  //Conditional Logic on Org Contact Info
+  const classes = useStyles();
+  const userRole = useRouteParams(AppChromeRoute).role;
+  const orgId = useRouteParams;
 
-  //Page should show the appropriate contact for the role of the user in each organization
+  const getOrganizationContacts = useQueryBundle(GetOrganizationContactsById, {
+    variables: { orgId: params.organizationId },
+  });
 
-  //If the user is both sub and employee in an organization then show both contacts with labels as shown
-
-  //Unless both contacts are the same, in which case just display the contact once with no label
-  //or if only one contact has data, in which case just display the one with no label
+  if (getOrganizationContacts.state === "LOADING") {
+    return <></>;
+  }
 
   return <></>;
 };
+
+const useStyles = makeStyles(theme => ({}));
