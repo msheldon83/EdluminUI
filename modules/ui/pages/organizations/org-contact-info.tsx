c7f334ba137633417afcf84d5edb3bb30fafbbc7
Allen Dzedzy <adzedzy@edustaff.org>
2020-04-03 13:48:04 -0400

    notes

--- modules/ui/pages/organizations/org-contact-info.tsx
+++ modules/ui/pages/organizations/org-contact-info.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
 
 export const OrganicationContactInfo: React.FC<{}> = props => {
+  //Conditional Logic on Org Contact Info
+
+  //Page should show the appropriate contact for the role of the user in each organization
+
+  //If the user is both sub and employee in an organization then show both contacts with labels as shown
+
+  //Unless both contacts are the same, in which case just display the contact once with no label
+  //or if only one contact has data, in which case just display the one with no label
+
   return <></>;
 };
