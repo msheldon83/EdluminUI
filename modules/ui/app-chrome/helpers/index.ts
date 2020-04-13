557b4bcabc77010f518c4a7b050dd8f0fdbb24dc
Chris Avenick <chris@stillrocksolutions.com>
2020-04-13 10:55:14 -0400

    Update index.ts

--- modules/ui/app-chrome/helpers/index.ts
+++ modules/ui/app-chrome/helpers/index.ts
@@ -1,5 +1,6 @@
 export const formatPhoneNumber = (phoneNumberString: string) => {
   const cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
   const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
   if (match) {
     return "(" + match[1] + ") " + match[2] + "-" + match[3];
