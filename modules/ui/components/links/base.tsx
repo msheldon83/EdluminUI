07dff247690ef4a612d6cdea07509e816e340a7c
cwstra <cwstra@gmail.com>
2020-04-16 16:53:02 -0400

    Proper theming

--- modules/ui/components/links/base.tsx
+++ modules/ui/components/links/base.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import clsx from 'clsx';
+import { makeStyles } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { Can } from "ui/components/auth/can";
 import { CanDo } from "ui/components/auth/types";
@@ -27,14 +29,16 @@ export const pickUrl = (s: string) => {
   };
 };
 
-export const BaseLink: React.FC<Props> = ({ permissions, to, children, linkClass, textClass, displayText = true}) => {
-  // These are the styles applied to the base mui link
-  const muiClass = "MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
-  const className = `${muiClass}${linkClass ? " "+linkClass: ""}`
+export const BaseLink: React.FC<Props> = ({ permissions, to, children, linkClass="", textClass="", displayText = true}) => {
+  const classes = useStyles();
   return (
   <>
     <Can do={permissions}>
-      <Link className={className} to={to}>
+      <Link className={clsx(
+        classes.root,
+        classes.underlineHover,
+        linkClass,
+        )} to={to}>
         {children}
       </Link>
     </Can>
@@ -46,3 +50,52 @@ export const BaseLink: React.FC<Props> = ({ permissions, to, children, linkClass
   </>
   )
 }
+
+// Classes are yoinked from M-UI's Link
+const useStyles = makeStyles(theme => ({
+  /* Styles applied to the root element. */
+  root: {
+    color: theme.customColors.blue,
+  },
+  /* Styles applied to the root element if `underline="none"`. */
+  underlineNone: {
+    textDecoration: 'none',
+  },
+  /* Styles applied to the root element if `underline="hover"`. */
+  underlineHover: {
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  /* Styles applied to the root element if `underline="always"`. */
+  underlineAlways: {
+    textDecoration: 'underline',
+  },
+  // Same reset as ButtonBase.root
+  /* Styles applied to the root element if `component="button"`. */
+  button: {
+    position: 'relative',
+    WebkitTapHighlightColor: 'transparent',
+    backgroundColor: 'transparent', // Reset default value
+    // We disable the focus ring for mouse, touch and keyboard users.
+    outline: 0,
+    border: 0,
+    margin: 0, // Remove the margin in Safari
+    borderRadius: 0,
+    padding: 0, // Remove the padding in Firefox
+    cursor: 'pointer',
+    userSelect: 'none',
+    verticalAlign: 'middle',
+    '-moz-appearance': 'none', // Reset
+    '-webkit-appearance': 'none', // Reset
+    '&::-moz-focus-inner': {
+      borderStyle: 'none', // Remove Firefox dotted outline.
+    },
+    '&$focusVisible': {
+      outline: 'auto',
+    },
+  },
+  /* Pseudo-class applied to the root element if the link is keyboard focused. */
+  focusVisible: {},
+}));
