72b1db50d2aafb701ecf38bbbdd12d6c303961f6
David Nawn <dnawn@edustaff.org>
2020-04-20 10:10:56 -0400

    Have the RoleContext return a specific type and start using it in the Can component

--- modules/ui/components/auth/can.tsx
+++ modules/ui/components/auth/can.tsx
@@ -2,18 +2,25 @@ import * as React from "react";
 import { useMyUserAccess } from "reference-data/my-user-access";
 import { can as CanHelper } from "helpers/permissions";
 import { useOrganizationId } from "core/org-context";
-import { CanDo } from "./types";
+import { CanDo, Role } from "./types";
+import { useRole } from "core/role-context";
 
 type Props = {
   do: CanDo;
   orgId?: string;
+  forRole?: Role;
   not?: boolean;
   context?: any;
 };
 
 export const Can: React.FC<Props> = props => {
   const canDoFn = useCanDo();
-  const canDoThis = canDoFn(props.do, props.orgId, props.context);
+  const canDoThis = canDoFn(
+    props.do,
+    props.orgId,
+    props.context,
+    props.forRole
+  );
 
   return canDoThis === !props.not ? <>{props.children}</> : null;
 };
@@ -21,13 +28,16 @@ export const Can: React.FC<Props> = props => {
 export const useCanDo = () => {
   const userAccess = useMyUserAccess();
   const contextOrgId = useOrganizationId();
+  const contextRole = useRole();
 
   const fn = (
     canDo: CanDo,
     orgId?: string | null | undefined,
-    context?: any
+    context?: any,
+    forRole?: Role | null | undefined
   ) => {
     orgId = orgId ?? contextOrgId;
+    forRole = forRole ?? contextRole;
     context = context ?? undefined;
 
     const canDoThis = CanHelper(
@@ -35,7 +45,8 @@ export const useCanDo = () => {
       userAccess?.permissionsByOrg ?? [],
       userAccess?.isSysAdmin ?? false,
       orgId ?? undefined,
-      context ?? undefined
+      context ?? undefined,
+      forRole ?? undefined
     );
 
     return canDoThis;
