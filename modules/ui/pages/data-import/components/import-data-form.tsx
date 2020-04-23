62def9d977df3bd23471e62a1f0a66371d166c63
Frank Dzedzy <fdzedzy@redroverk12.com>
2020-04-23 10:49:48 -0400

    Add create import functionality

--- modules/ui/pages/data-import/components/import-data-form.tsx
+++ modules/ui/pages/data-import/components/import-data-form.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
-import { Typography } from "@material-ui/core";
+import { useState } from "react";
+import {
+  Button,
+  Grid,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  TextField,
+} from "@material-ui/core";
+import * as yup from "yup";
 import { useTranslation } from "react-i18next";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Formik } from "formik";
 import { makeStyles } from "@material-ui/core";
 import { ShowErrors } from "ui/components/error-helpers";
 import { useSnackbar } from "hooks/use-snackbar";
+import { useMutationBundle } from "graphql/hooks";
+import {
+  SaveDataImportMutation,
+  SaveDataImportInput,
+  formSerializer,
+} from "../graphql/save-data-import";
+import { ImportTypeFilter } from "./import-type-filter";
+import { DataImportType } from "graphql/server-types.gen";
+import { ActionButtons } from "ui/components/action-buttons";
 
 type Props = {
   orgId: string;
@@ -16,17 +33,199 @@ type Props = {
 export const ImportDataForm: React.FC<Props> = props => {
   const { t } = useTranslation();
   const classes = useStyles();
+  const { openSnackbar } = useSnackbar();
+
+  const [fileToSave, setFileToSave] = useState<File>();
+
+  const [saveDataImport] = useMutationBundle(SaveDataImportMutation, {
+    onError: error => {
+      ShowErrors(error, openSnackbar); // TODO: Parse error codes into better messages
+    },
+  });
+
+  const onSaveDataImport = async (input: SaveDataImportInput) => {
+    const result = await saveDataImport({
+      variables: {
+        input: input,
+        formSerializer: formSerializer,
+      },
+    });
+    if (props.refetchImports) {
+      await props.refetchImports();
+    }
+    if (result.data?.saveDataImport.isSuccess) {
+      return true;
+    } else {
+      // This will show parse errors since parse returns a 200
+      openSnackbar({
+        message: result.data?.saveDataImport.message,
+        dismissable: true,
+        status: "error",
+      });
+      return false;
+    }
+  };
 
   return (
     <>
       <Formik
-        initialValues={{}}
-        onReset={(values, formProps) => {}}
-        onSubmit={async (data: any, formProps) => {}}
+        initialValues={{
+          importTypeId: DataImportType.Employee,
+          file: fileToSave,
+          importAction: "validate" as "validate" | "import" | "parse",
+          notificationEmailAddresses: "",
+        }}
+        onReset={(values, formProps) => {
+          formProps.setFieldValue("importTypeId", DataImportType.Employee);
+          formProps.setFieldValue("file", undefined);
+          setFileToSave(undefined);
+          formProps.setFieldValue("importAction", "validate");
+          formProps.setFieldValue("notificationEmailAddresses", "");
+          props.onClose();
+        }}
+        onSubmit={async (data: any, formProps) => {
+          const result = await onSaveDataImport({
+            importOptions: {
+              orgId: props.orgId,
+              dataImportTypeId: data.importTypeId,
+              parseOnly: data.importAction === "parse",
+              validateOnly: data.importAction === "validate",
+              notificationEmailAddresses:
+                data.notificationEmailAddresses.length > 0
+                  ? data.notificationEmailAddresses
+                      .replace(/\s/g, "")
+                      .split(",")
+                  : undefined,
+            },
+            file: data.file,
+          });
+          if (result) {
+            formProps.resetForm();
+            props.onClose();
+          }
+        }}
+        validationSchema={yup.object().shape({
+          notificationEmailAddresses: yup
+            .array()
+            .transform(function(value, originalValue) {
+              if (this.isType(value) && value !== null) {
+                return value;
+              }
+              return originalValue ? originalValue.split(/[\s,]+/) : [];
+            })
+            .of(
+              yup.string().email(({ value }) => `${value} is not a valid email`)
+            ),
+          file: yup
+            .mixed()
+            .required(t("A file is required"))
+            .test(
+              "fileType",
+              "Only CSV files are supported",
+              value =>
+                value &&
+                value.name
+                  .split(".")
+                  .pop()
+                  .toLowerCase() === "csv"
+            ),
+        })}
       >
-        {({ values, handleSubmit, setFieldValue, submitForm, handleReset }) => (
+        {({
+          values,
+          handleSubmit,
+          setFieldValue,
+          submitForm,
+          handleReset,
+          resetForm,
+          errors,
+        }) => (
           <form className={classes.form} onSubmit={handleSubmit}>
-            <Typography variant="h5">{t("It's coming soon!")}</Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={3}>
+                <ImportTypeFilter
+                  includeAllOption={false}
+                  selectedTypeId={values.importTypeId}
+                  setSelectedTypeId={(typeId?: DataImportType) => {
+                    if (typeId) setFieldValue("importTypeId", typeId);
+                  }}
+                />
+                <div className={classes.fileUpload}>
+                  <input
+                    type="file"
+                    id="contained-button-file"
+                    accept=".csv"
+                    className={classes.input}
+                    onChange={event => {
+                      if (event.target.files && event.target.files.length > 0) {
+                        setFileToSave(event.target.files[0]);
+                        setFieldValue("file", event.target.files[0]);
+                      }
+                    }}
+                  />
+                  <label htmlFor="contained-button-file">
+                    <Button variant="outlined" component="span">
+                      {t("Attach file")}
+                    </Button>
+                  </label>
+                  <div>{values.file && values.file.name}</div>
+                  {errors.file && (
+                    <div className={classes.errorMsg}>{errors.file}</div>
+                  )}
+                </div>
+              </Grid>
+              <Grid item xs={2}>
+                <div className={classes.label}>{t("Import action")}</div>
+                <RadioGroup
+                  value={values.importAction}
+                  onChange={e => setFieldValue("importAction", e.target.value)}
+                >
+                  <FormControlLabel
+                    value={"parse"}
+                    control={<Radio color="primary" />}
+                    label={t("Parse only")}
+                    labelPlacement="end"
+                  />
+                  <FormControlLabel
+                    value={"validate"}
+                    control={<Radio color="primary" />}
+                    label={t("Validate only")}
+                    labelPlacement="end"
+                  />
+                  <FormControlLabel
+                    value={"import"}
+                    control={<Radio color="primary" />}
+                    label={t("Import")}
+                    labelPlacement="end"
+                  />
+                </RadioGroup>
+              </Grid>
+              <Grid item xs={4}>
+                <div className={classes.label}>
+                  {t("Email addresses to notify")}
+                </div>
+                <TextField
+                  name={"notificationEmailAddresses"}
+                  value={values.notificationEmailAddresses}
+                  multiline
+                  rows="6"
+                  variant="outlined"
+                  fullWidth
+                  onChange={e =>
+                    setFieldValue("notificationEmailAddresses", e.target.value)
+                  }
+                  placeholder={t(
+                    "email1@redroverk12.com, email2@redroverk12.com"
+                  )}
+                  error={errors.notificationEmailAddresses ? true : false}
+                  helperText={errors.notificationEmailAddresses}
+                />
+              </Grid>
+            </Grid>
+            <ActionButtons
+              submit={{ text: t("Import"), execute: submitForm }}
+              cancel={{ text: t("Cancel"), execute: handleReset }}
+            />
           </form>
         )}
       </Formik>
@@ -41,5 +240,20 @@ export const useStyles = makeStyles(theme => ({
   details: {
     padding: 0,
     display: "block",
+  },
+  input: {
+    display: "none",
+  },
+  label: {
+    color: theme.customColors.eduBlack,
+    fontSize: theme.typography.pxToRem(14),
+    marginBottom: theme.spacing(0.4),
+    lineHeight: theme.typography.pxToRem(21),
+  },
+  fileUpload: {
+    paddingTop: theme.spacing(2),
+  },
+  errorMsg: {
+    color: theme.customColors.darkRed,
   },
 }));
