54e40083ad8647a13f97e18213ea3e215fab6c9d
Allen Dzedzy <adzedzy@edustaff.org>
2020-04-23 11:10:59 -0400

    Delete mutation added & tested

--- modules/ui/pages/school-groups/components/location-group-locations.tsx
+++ modules/ui/pages/school-groups/components/location-group-locations.tsx
@@ -11,26 +11,25 @@ type Props = { locations: Location[] };
 export const LocationGroupLocations: React.FC<Props> = props => {
   const { t } = useTranslation();
   const locations = props.locations;
+
   return (
     <Section>
       <SectionHeader title={t("Schools")} />
       <Grid container spacing={2}>
         <Grid container item spacing={2} xs={5}>
-          {locations.length > 0 && (
-            <Grid item xs={12} sm={6} lg={6}>
-              {locations.length === 0 ? (
-                <div>{t("Not defined")}</div>
-              ) : (
-                locations.map((n, i) => (
-                  <div key={i}>
-                    <LocationLink orgId={n.orgId} locationId={n.id}>
-                      {n.name}
-                    </LocationLink>
-                  </div>
-                ))
-              )}
-            </Grid>
-          )}
+          <Grid item xs={12} sm={6} lg={6}>
+            {locations.length === 0 ? (
+              <div>{t("Not defined")}</div>
+            ) : (
+              locations.map((n, i) => (
+                <div key={i}>
+                  <LocationLink orgId={n.orgId} locationId={n.id}>
+                    {n.name}
+                  </LocationLink>
+                </div>
+              ))
+            )}
+          </Grid>
         </Grid>
       </Grid>
     </Section>
--- modules/ui/pages/school-groups/view.tsx
+++ modules/ui/pages/school-groups/view.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
-import { useQueryBundle } from "graphql/hooks";
-import { LocationGroupInformation } from "./components/information";
+import { useQueryBundle, useMutationBundle } from "graphql/hooks";
 import { SubstitutePrefCard } from "ui/components/sub-pools/subpref-card";
 import { GetLocationGroupById } from "./graphql/get-location-groups-by-id.gen";
 import { useRouteParams } from "ui/routes/definition";
-import { LocationGroupViewRoute } from "ui/routes/location-groups";
+import {
+  LocationGroupViewRoute,
+  LocationGroupsRoute,
+} from "ui/routes/location-groups";
 import { useTranslation } from "react-i18next";
 import { Location as Loc, PermissionEnum } from "graphql/server-types.gen";
 import { PageHeader } from "ui/components/page-header";
 import { makeStyles } from "@material-ui/core";
+import { useHistory } from "react-router";
 import { OrgUserPermissions, Role } from "ui/components/auth/types";
 import { can } from "helpers/permissions";
 import { LocationGroupSubPrefRoute } from "ui/routes/location-groups";
+import { useSnackbar } from "hooks/use-snackbar";
+import { ShowErrors } from "ui/components/error-helpers";
 import { LocationGroupLocations } from "./components/location-group-locations";
+import { DeleteLocationGroup } from "./graphql/delete-location-group.gen";
 
 export const LocationGroupViewPage: React.FC<{}> = props => {
   const params = useRouteParams(LocationGroupViewRoute);
   const { t } = useTranslation();
   const classes = useStyles();
+  const history = useHistory();
+  const { openSnackbar } = useSnackbar();
 
   const getLocationGroup = useQueryBundle(GetLocationGroupById, {
     variables: {
@@ -25,6 +33,25 @@ export const LocationGroupViewPage: React.FC<{}> = props => {
     },
     fetchPolicy: "cache-first",
   });
+
+  const [deleteLocationGroupMutation] = useMutationBundle(DeleteLocationGroup, {
+    onError: error => {
+      ShowErrors(error, openSnackbar);
+    },
+  });
+  const deleteLocationGroup = React.useCallback(async () => {
+    const result = await deleteLocationGroupMutation({
+      variables: {
+        locationGroupId: params.locationGroupId,
+      },
+      awaitRefetchQueries: true,
+      refetchQueries: ["GetAllLocationGroupsWithinOrg"],
+    });
+
+    if (!result.errors) {
+      history.push(LocationGroupsRoute.generate(params));
+    }
+  }, [deleteLocationGroupMutation, history, params]);
 
   if (getLocationGroup.state === "LOADING") {
     return <></>;
@@ -54,6 +81,17 @@ export const LocationGroupViewPage: React.FC<{}> = props => {
             forRole
           )
         }
+        actions={[
+          {
+            name: t("Change History"),
+            onClick: () => {},
+          },
+          {
+            name: t("Delete"),
+            onClick: deleteLocationGroup,
+            permissions: [PermissionEnum.LocationGroupDelete],
+          },
+        ]}
       />
       <PageHeader
         text={locationGroup.externalId}
