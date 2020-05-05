4fd5921f941e6d1c7dba77be0a34ad4e562aeeef
Corin (CWStra) <cwstra@gmail.com>
2020-05-05 16:12:46 -0400

    Add target to LinkOptions

--- modules/ui/components/links/base.tsx
+++ modules/ui/components/links/base.tsx
@@ -11,6 +11,7 @@ export type LinkOptions = {
   color?: "blue" | "black";
   displayText?: boolean;
   disabled?: boolean;
+  target?: "_self" | "_blank";
 };
 
 type Props = {
@@ -43,6 +44,7 @@ export const BaseLink: React.FC<Props> = ({
   displayText = true,
   color = "blue",
   disabled,
+  target = "_self",
 }) => {
   const classes = useStyles();
   const text = <span className={textClass}>{children}</span>;
@@ -58,6 +60,7 @@ export const BaseLink: React.FC<Props> = ({
             classes[color]
           )}
           to={to}
+          target={target}
         >
           {children}
         </Link>
