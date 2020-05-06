eed56a2a21703a519de4f40a5675272427ef7970
Frank Dzedzy <fdzedzy@redroverk12.com>
2020-05-06 10:38:27 -0400

    Fun unauthorized page

--- modules/ui/pages/unauthorized/index.tsx
+++ modules/ui/pages/unauthorized/index.tsx
@@ -1,25 +1,94 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { Typography, makeStyles } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
+import { useHistory } from "react-router";
+import { useIsMobile } from "hooks";
 
 export const Unauthorized: React.FC<{}> = () => {
   const { t } = useTranslation();
   const classes = useStyles();
+  const history = useHistory();
+  const isMobile = useIsMobile();
 
-  return (
-    <div className={classes.message}>
-      <Typography variant={"h1"}>
-        {t("You do not have access to this page")}
-      </Typography>
+  return isMobile ? (
+    <div className={classes.container}>
+      <div className={classes.mobileContainer}>
+        <div className={classes.unauthorizedText}>{t("UNAUTHORIZED")}</div>
+        <div className={classes.mobileText}>
+          {t("You're not supposed to be here")}
+        </div>
+        <img src={require("./images/mobileMadCat.svg")} />
+        <Button
+          variant="contained"
+          className={classes.button}
+          onClick={() => history.goBack()}
+        >
+          {t("Go back")}
+        </Button>
+      </div>
+    </div>
+  ) : (
+    <div className={classes.desktopContainer}>
+      <div className={classes.textContainer}>
+        <div className={classes.unauthorizedText}>{t("UNAUTHORIZED")}</div>
+        <div className={classes.text}>
+          {t("You're not supposed to be here")}
+        </div>
+        <Button
+          variant="contained"
+          className={classes.button}
+          onClick={() => history.goBack()}
+        >
+          {t("Go back")}
+        </Button>
+      </div>
+      <img src={require("./images/madCat.svg")} />
     </div>
   );
 };
 
 const useStyles = makeStyles(theme => ({
-  message: {
+  container: {
+    textAlign: "center",
+  },
+  mobileContainer: {
+    width: "300px",
+    textAlign: "center",
+    display: "inline-block",
+    marginTop: theme.spacing(4),
+  },
+  desktopContainer: {
+    marginTop: theme.spacing(20),
+    marginLeft: theme.spacing(5),
     display: "flex",
-    justifyContent: "center",
-    width: "100%",
-    marginTop: theme.spacing(5),
+  },
+  textContainer: {
+    width: "300px",
+  },
+  unauthorizedText: {
+    display: "inline-block",
+    color: theme.customColors.black,
+    fontSize: theme.typography.pxToRem(14),
+    fontWeight: "bold",
+    paddingBottom: theme.spacing(2),
+  },
+  text: {
+    color: theme.customColors.black,
+    fontSize: theme.typography.pxToRem(48),
+    fontWeight: 600,
+    wordWrap: "normal",
+  },
+  mobileText: {
+    color: theme.customColors.black,
+    fontSize: theme.typography.pxToRem(24),
+    fontWeight: 600,
+    wordWrap: "normal",
+    width: "160px",
+    margin: "0 auto",
+  },
+  button: {
+    display: "inline-block",
+    backgroundColor: theme.customColors.black,
+    marginTop: theme.spacing(2),
   },
 }));
