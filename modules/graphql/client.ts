25c8fe06148d9935d634ecc011f0052d1459cef5
David Nawn <dnawn@edustaff.org>
2020-05-07 14:51:52 -0400

    Adding support to the RestLink to handle downloading a file. When needed just need to define an endpoint here and follow this example.

--- modules/graphql/client.ts
+++ modules/graphql/client.ts
@@ -17,7 +17,16 @@ export function buildGraphqlClient(opts: {
     ...opts,
   };
   const cache = new InMemoryCache();
-  const restLink = new RestLink({ uri: Config.restUri });
+  const restLink = new RestLink({
+    uri: Config.restUri,
+    endpoints: {
+      reportExport: {
+        uri: "/api/report/export",
+        responseTransformer: async (data: any, typeName: string) =>
+          fileDownloadResponseTransformer(data, "Report"),
+      },
+    },
+  });
   const links = [...compact([prefixLink]), restLink, createHttpLink({ uri })];
   const link = ApolloLink.from(links);
   const client = new ApolloClient({
@@ -33,3 +42,50 @@ export function buildGraphqlClient(opts: {
 
   return client;
 }
+
+const fileDownloadResponseTransformer = async (
+  response: any,
+  defaultFileName?: string
+) => {
+  if (response.ok) {
+    // Get the filename out of the header
+    const contentDispositionHeader =
+      response.headers.get("Content-Disposition")?.toString() ?? "";
+    const contentDispositionPieces = contentDispositionHeader
+      .split(";")
+      .map((c: any) => c.toString().trim());
+    const filename =
+      contentDispositionPieces
+        .find((c: any) => c.toString().startsWith("filename="))
+        ?.replace("filename=", "") ??
+      defaultFileName ??
+      "file";
+
+    // Get the content type out of the header so we can use the right file extension
+    const contentTypeHeader = response.headers.get("Content-Type")?.toString();
+    let fileExtension = "csv";
+    switch (contentTypeHeader) {
+      case "application/pdf":
+        fileExtension = "pdf";
+        break;
+      case "text/csv":
+        fileExtension = "csv";
+        break;
+    }
+
+    return response.blob().then((b: any) => {
+      const url = window.URL.createObjectURL(b);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `${filename}.${fileExtension}`;
+      // We need to append the element to the dom -> otherwise it will not work in firefox
+      document.body.appendChild(a);
+      a.click();
+      // Afterwards we remove the element again
+      a.remove();
+      // Return an empty object to make the Apollo Client happy
+      return {};
+    });
+  }
+  return Promise.reject(`Could Not Download File`);
+};
