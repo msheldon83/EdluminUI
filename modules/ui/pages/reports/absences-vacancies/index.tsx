eb623e790bfd816861a658a01656dcf44018d147
David Nawn <dnawn@edustaff.org>
2020-05-08 10:10:18 -0400

    Memoizing some things/cleanup

--- modules/ui/pages/reports/absences-vacancies/index.tsx
+++ modules/ui/pages/reports/absences-vacancies/index.tsx
@@ -12,53 +12,43 @@ import { addDays } from "date-fns";
 export const AbsencesVacanciesReport: React.FC<{}> = () => {
   const { t } = useTranslation();
 
-  const reportInput: ReportDefinitionInput = {
-    from: "AbsenceAndVacancy",
-    select: [
-      "ConfirmationNumber",
-      "Date",
-      "LocationName",
-      "Concat(AbsentEmployeeFirstName,' ',AbsentEmployeeLastName) AS Employee",
-      "AbsStartTime",
-      "AbsEndTime",
-      "ReasonName",
-      "Concat(SubFirstName,' ',SubLastName) AS Substitute",
-      "SubStartTime",
-      "SubEndTime",
-      "PayDays",
-      "PayHours",
-      "Title",
-      "PositionTypeName",
-      "RequiresSub",
-      "IsFilled",
-    ],
-    filter: [
-      {
-        fieldName: "Date",
-        expressionFunction: ExpressionFunction.Between,
-        value: [addDays(new Date(), -7), new Date()],
-        isRequired: true,
-      },
-    ],
-    orderBy: [
-      {
-        expression: "Date",
-        direction: Direction.Desc,
-      },
-    ],
-  };
-
-  const filterFieldsOverride = [
-    "Date",
-    "AbsentEmployeeId",
-    "SubEmployeeId",
-    "PositionTypeId",
-    "LocationId",
-    "AbsenceReasonId",
-    "VacancyReasonId",
-    "IsAbsence",
-    "IsVacancy",
-  ];
+  const reportInput: ReportDefinitionInput = React.useMemo(() => {
+    return {
+      from: "AbsenceAndVacancy",
+      select: [
+        "ConfirmationNumber",
+        "Date",
+        "LocationName",
+        "Concat(AbsentEmployeeFirstName,' ',AbsentEmployeeLastName) AS Employee",
+        "AbsStartTime",
+        "AbsEndTime",
+        "ReasonName",
+        "Concat(SubFirstName,' ',SubLastName) AS Substitute",
+        "SubStartTime",
+        "SubEndTime",
+        "PayDays",
+        "PayHours",
+        "Title",
+        "PositionTypeName",
+        "RequiresSub",
+        "IsFilled",
+      ],
+      filter: [
+        {
+          fieldName: "Date",
+          expressionFunction: ExpressionFunction.Between,
+          value: [addDays(new Date(), -7), new Date()],
+          isRequired: true,
+        },
+      ],
+      orderBy: [
+        {
+          expression: "Date",
+          direction: Direction.Desc,
+        },
+      ],
+    };
+  }, []);
 
   return (
     <>
@@ -66,7 +56,17 @@ export const AbsencesVacanciesReport: React.FC<{}> = () => {
       <Report
         input={reportInput}
         exportFilename={t("AbsencesVacanciesReport")}
-        filterFieldsOverride={filterFieldsOverride}
+        filterFieldsOverride={[
+          "Date",
+          "AbsentEmployeeId",
+          "SubEmployeeId",
+          "PositionTypeId",
+          "LocationId",
+          "AbsenceReasonId",
+          "VacancyReasonId",
+          "IsAbsence",
+          "IsVacancy",
+        ]}
       />
     </>
   );
