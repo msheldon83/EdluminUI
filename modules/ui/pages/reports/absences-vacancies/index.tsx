f9ce8de3c6f8f305fcab092ed539797fcdcf70c9
David Nawn <dnawn@edustaff.org>
2020-05-08 09:35:22 -0400

    Bring in the new Notes fields to the Absences & Vacancies report

--- modules/ui/pages/reports/absences-vacancies/index.tsx
+++ modules/ui/pages/reports/absences-vacancies/index.tsx
@@ -31,6 +31,9 @@ export const AbsencesVacanciesReport: React.FC<{}> = () => {
       "PositionTypeName",
       "RequiresSub",
       "IsFilled",
+      "NotesToAdmin",
+      "AdminOnlyNotes",
+      "NotesToReplacement",
     ],
     filter: [
       {
