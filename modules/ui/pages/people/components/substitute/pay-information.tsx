ef385d885f6793d5c6633f29f8a9250d18587f43
Allen Dzedzy <adzedzy@edustaff.org>
2020-05-11 14:12:45 -0400

    PR tweaks.

--- modules/ui/pages/people/components/substitute/pay-information.tsx
+++ modules/ui/pages/people/components/substitute/pay-information.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Typography, Grid, makeStyles } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
 import { Section } from "ui/components/section";
 import { SectionHeader } from "ui/components/section-header";
 import { useTranslation } from "react-i18next";
 import { Formik } from "formik";
 import { OptionType, SelectNew } from "ui/components/form/select-new";
 import { PayCode, PermissionEnum } from "graphql/server-types.gen";
-import { OptionTypeBase } from "react-select/src/types";
 
 export const editableSections = {
   payInformation: "edit-pay-information",
@@ -16,25 +15,23 @@ type Props = {
   editing: string | null;
   editable: boolean;
   payCodeOptions: OptionType[];
+  onSubmit: (orgUser: any) => Promise<unknown>;
   payCode?: Pick<PayCode, "id" | "name"> | null;
-  setEditing?: React.Dispatch<React.SetStateAction<string | null>>;
-  onSubmit?: (orgUser: any) => Promise<unknown>;
+  setEditing: React.Dispatch<React.SetStateAction<string | null>>;
   onCancel?: () => void;
   editPermissions?: PermissionEnum[];
 };
 
 export const SubPayInformation: React.FC<Props> = props => {
   const { t } = useTranslation();
-  const classes = useStyles();
-
   const payCodeName = props?.payCode?.name ?? t("None Selected");
 
   return (
     <>
       <Formik
         onSubmit={async (data, e) => {
-          await props.onSubmit!({
-            payCodeId: data.payCodeId === "" ? null : data.payCodeId,
+          await props.onSubmit({
+            payCodeId: data.payCodeId,
           });
         }}
         initialValues={{ payCodeId: props.payCode?.id }}
@@ -49,7 +46,7 @@ export const SubPayInformation: React.FC<Props> = props => {
                     text: t("Edit"),
                     visible: !props.editing && props.editable,
                     execute: () => {
-                      props.setEditing!(editableSections.payInformation);
+                      props.setEditing(editableSections.payInformation);
                     },
                     permissions: props.editPermissions,
                   },
@@ -63,7 +60,7 @@ export const SubPayInformation: React.FC<Props> = props => {
                   text: t("Cancel"),
                   visible: props.editing === editableSections.payInformation,
                   execute: () => {
-                    props.setEditing!(null);
+                    props.setEditing(null);
                   },
                 }}
               />
@@ -77,16 +74,8 @@ export const SubPayInformation: React.FC<Props> = props => {
                       ) ?? { value: "", label: "" }
                     }
                     multiple={false}
-                    onChange={(e: OptionType) => {
-                      let selectedValue = null;
-                      if (e) {
-                        if (Array.isArray(e)) {
-                          selectedValue = (e as Array<OptionTypeBase>)[0].value;
-                        } else {
-                          selectedValue = (e as OptionTypeBase).value;
-                        }
-                      }
-                      setFieldValue("payCodeId", selectedValue);
+                    onChange={(value: OptionType) => {
+                      setFieldValue("payCodeId", value.value);
                     }}
                     options={props.payCodeOptions}
                   />
@@ -104,12 +93,3 @@ export const SubPayInformation: React.FC<Props> = props => {
     </>
   );
 };
-
-export const useStyles = makeStyles(theme => ({
-  heading: {
-    fontSize: theme.typography.pxToRem(16),
-    fontWeight: "bold",
-    letterSpacing: theme.typography.pxToRem(0.15),
-    color: theme.customColors.blue,
-  },
-}));
