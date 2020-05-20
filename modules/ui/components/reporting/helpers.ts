da0d017da3605a32883f7d29a9446407302629db
David Nawn <dnawn@edustaff.org>
2020-05-20 11:34:56 -0400

    Update report components to respect the new ReportInput SelectField configurations

--- modules/ui/components/reporting/helpers.ts
+++ modules/ui/components/reporting/helpers.ts
@@ -1,5 +1,6 @@
 import { Index } from "react-virtualized";
-import { Row, DataExpression, GraphType } from "./types";
+import { Row, DataExpression, GraphType, SelectField } from "./types";
+import { compact } from "lodash-es";
 
 export const calculateRowHeight = ({ index }: Index, rows: Row[]) => {
   const row = rows[index];
@@ -12,14 +13,55 @@ export const calculateRowHeight = ({ index }: Index, rows: Row[]) => {
 export const calculateColumnWidth = (
   { index }: Index,
   isGrouped: boolean,
-  dataColumnIndexMap: Record<string, DataExpression>
+  dataColumnIndexMap: Record<string, DataExpression>,
+  inputSelects: SelectField[]
 ) => {
   if (isGrouped && index === 0) {
     return 300;
   }
-  return (
-    dataColumnIndexMap[index].dataSourceField?.defaultColumnWidthInPixels ?? 200
+
+  const visibleMap = getVisibleDataColumnIndexMap(
+    dataColumnIndexMap,
+    inputSelects
   );
+
+  return (
+    inputSelects.filter(s => !s.hiddenFromReport)[index]?.width ??
+    visibleMap[index]?.dataSourceField?.defaultColumnWidthInPixels ??
+    200
+  );
+};
+
+export const getVisibleData = (
+  data: any[],
+  inputSelects: SelectField[]
+): any[] => {
+  const visibleData: any[] = [];
+  data.forEach((d, i) => {
+    if (!inputSelects[i]?.hiddenFromReport) {
+      visibleData.push(d);
+    }
+  });
+  return visibleData;
+};
+
+export const getVisibleDataColumnIndexMap = (
+  dataColumnIndexMap: Record<string, DataExpression>,
+  inputSelects: SelectField[]
+): Record<string, DataExpression> => {
+  let object: Record<string, DataExpression> = {};
+  let key = 0;
+  Object.entries(dataColumnIndexMap).forEach((d, i) => {
+    if (!inputSelects[i]?.hiddenFromReport) {
+      const value = d[1];
+      object = {
+        ...object,
+        [key.toString()]: value,
+      };
+      key = key + 1;
+    }
+  });
+  return object;
 };
 
 export const getGraphTypeString = (graphType: GraphType): string | null => {
