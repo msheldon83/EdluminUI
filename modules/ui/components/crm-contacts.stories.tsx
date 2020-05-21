bdba45dec316cd01b87a60af16d9a0c93f15f34e
Marshall Lentz <mlentz@redroverK12.com>
2020-05-21 13:54:07 -0400

    Finish contact block for CRM page

--- modules/ui/components/crm-contacts.stories.tsx
+++ modules/ui/components/crm-contacts.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/styles";
 
-export const CRMStory = () => {
+export const CRMContacts = () => {
   const classes = useStyles();
 
   return (
@@ -14,7 +14,7 @@ export const CRMStory = () => {
           <div className={classes.role}>Director of HR</div>
           <div className={classes.email}>msmith@reallycoolschools.edu</div>
           <div className={classes.phone}>(610) 555-1212</div>
-          <div className={classes.edit}>Edit</div>
+          <div className={classes.textLink}>Edit</div>
         </div>
         <div className={classes.contactSubfield}>
           <div className={classes.title}>Billing contact</div>
@@ -22,14 +22,14 @@ export const CRMStory = () => {
           <div className={classes.role}>Accounts Payable</div>
           <div className={classes.email}>jkappadicco@reallycoolschools.edu</div>
           <div className={classes.phone}>(610) 555-1212</div>
-          <div className={classes.edit}>Edit</div>
-          <div className={classes.invite}>Invite to login</div>
+          <div className={classes.textLink}>Edit</div>
+          <div className={classes.textLink}>Invite to login</div>
         </div>
         <div className={classes.contactSubfield}>
           <div className={classes.title}>Billing address</div>
           <div className={classes.addressLine}>1265 E Notting Drive</div>
           <div className={classes.addressLine}>Exton, PA 19341</div>
-          <div className={classes.edit}>Edit</div>
+          <div className={classes.textLink}>Edit</div>
         </div>
       </div>
     </React.Fragment>
@@ -38,13 +38,16 @@ export const CRMStory = () => {
 
 const useStyles = makeStyles(theme => ({
   contacts: {
+    backgroundColor: "#FFFFFF",
     display: "block",
+    border: "1px solid #E5E5E5",
+    borderRadius: "4px",
     margin: "40px 20px 0px 20px",
-    border: "1px solid black",
   },
   header: {
     fontSize: "24px",
     lineHeight: "36px",
+    margin: "10px 0px 0px 10px",
   },
   contactSubfield: {
     display: "inline-block",
@@ -55,31 +58,33 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "bold",
   },
   name: {
-    padding: "5px",
+    // padding: "5px",
+    lineHeight: "22px",
   },
   role: {
-    padding: "5px",
+    // padding: "5px",
+    lineHeight: "22px",
   },
   email: {
-    padding: "5px",
+    // padding: "5px",
+    lineHeight: "22px",
   },
   phone: {
-    padding: "5px",
+    // padding: "5px",
+    lineHeight: "22px",
   },
   addressLine: {
-    padding: "5px",
+    // padding: "5px",
+    lineHeight: "22px",
   },
-  edit: {
+  textLink: {
     color: "#FF5555",
+    fontSize: "14px",
+    lineHeight: "22px",
     textDecoration: "underline",
     textDecorationColor: "#FF5555",
     display: "inline-block",
+    marginTop: "10px",
     marginRight: "30px",
-  },
-  invite: {
-    color: "#FF5555",
-    textDecoration: "underline",
-    textDecorationColor: "#FF5555",
-    display: "inline-block",
   },
 }));
