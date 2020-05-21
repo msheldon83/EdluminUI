ddc225cf3b1b733eee7752f16a1c9c7c16ed7b63
Scott Corgan <scottcorgan@gmail.com>
2020-05-21 08:48:15 -0400

    Add number input component

--- modules/ui/components/form/index.stories.tsx
+++ modules/ui/components/form/index.stories.tsx
@@ -7,5 +7,6 @@ export { DateInputStory } from "./date-input.stories";
 export { PhoneInputStory } from "./phone-input.stories";
 export { Select, SelectNew, MultiSelectNew } from "./select.stories";
 export { TimeInput } from "./time-input.stories";
+export { NumberInputStory } from "./number-input.stories";
 export { DurationInput } from "./duration-input.stories";
 export { AccountingCodeDropdownStory } from "./accounting-code-dropdown.stories";
