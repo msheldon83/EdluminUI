5510bb0cca7f08130b73af8e36c003043ece4cab
Marshall Lentz <mlentz@redroverK12.com>
2020-05-22 16:55:05 -0400

    Add CRM subscription to storybook

---
+++ modules/ui/components/crm-subscription.stories.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+export const CRMSubscription = () => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <div className={classes.container}>
+        <div className={classes.header}>Subscription for 2020-21</div>
+        <table className={classes.subscriptionTable}>
+          <thead className={classes.evenRow}>
+            <td>Item</td>
+            <td>Rate</td>
+            <td>Qty</td>
+            <td>In use</td>
+            <td>Total /mo</td>
+          </thead>
+          <tbody>
+            <tr className={classes.oddRow}>
+              <td>Employees requiring a substitute</td>
+              <td>$1.53/mo</td>
+              <td>203</td>
+              <td>200 (-3)</td>
+              <td>$310.59</td>
+            </tr>
+            <tr className={classes.evenRow}>
+              <td>Employees not requiring a substitute</td>
+              <td>$0.73/mo</td>
+              <td>0</td>
+              <td>5 (+5)</td>
+              <td>$0.00</td>
+            </tr>
+            <tr className={classes.oddRow}>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>$310.59</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    backgroundColor: "#FFFFFF",
+  },
+  header: {
+    fontSize: "24px",
+    lineHeight: "36px",
+  },
+  subscriptionTable: {
+    border: "1px solid black",
+  },
+  evenRow: {
+    backgroundColor: "#E5E5E5",
+  },
+  oddRow: {
+    backgroundColor: "#FFFFFF",
+  },
+}));
--- modules/ui/components/index.stories.tsx
+++ modules/ui/components/index.stories.tsx
@@ -6,3 +6,4 @@ export { ActiveInactiveFilterStory } from "./active-inactive-filter.stories";
 export { ActivityLogStory } from "./activity-log/activity-log-stories";
 export { CRMContacts } from "./crm-contacts.stories";
 export { CRMContactsReact } from "./crm-contacts.stories";
+export { CRMSubscription } from "./crm-subscription.stories";
