e355703155a3a68b51ad658697f621d924c9b834
Chris Avenick <chris@stillrocksolutions.com>
2020-05-27 15:19:24 -0400

    update to support all position type absence reasons

--- modules/ui/pages/position-type/components/position-type-abs-reasons-card.tsx
+++ modules/ui/pages/position-type/components/position-type-abs-reasons-card.tsx
@@ -5,6 +5,7 @@ import {
   Checkbox,
   Chip,
   makeStyles,
+  Divider,
 } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { PositionType, PermissionEnum } from "graphql/server-types.gen";
@@ -23,6 +24,7 @@ import { useMutationBundle } from "graphql/hooks";
 import { UpdateAbsenceReason } from "ui/pages/absence-reason/graphql/update-absence-reason.gen";
 import { ShowErrors } from "ui/components/error-helpers";
 import { useSnackbar } from "hooks/use-snackbar";
+import { AbsenceReason } from "ui/pages/absence-reason";
 
 type Props = {
   positionTypeId: string;
@@ -40,11 +42,18 @@ export const PositionTypeAbsReasonsCard: React.FC<Props> = props => {
 
   const absenceReasons = useAbsenceReasons(params.organizationId);
 
-  const [filteredAbsenceReasons, setFilteredAbsenceReasons] = React.useState<
-    any[]
-  >();
+  const [
+    filteredAbsenceReasonIds,
+    setFilteredAbsenceReasonIds,
+  ] = React.useState<any[]>();
 
-  const absenceReasonOptions = useAbsenceReasonOptions(params.organizationId);
+  const allPositionTypeAbsenceReasons = absenceReasons.filter(
+    ar => ar.allPositionTypes
+  );
+
+  const absenceReasonOptions = absenceReasons
+    .filter(ar => !ar.allPositionTypes)
+    .map(r => ({ label: r.name, value: r.id }));
 
   const [updateAbsenceReason] = useMutationBundle(UpdateAbsenceReason, {
     onError: error => {
@@ -55,62 +64,63 @@ export const PositionTypeAbsReasonsCard: React.FC<Props> = props => {
   const handleUpdateAbsenceReason = async (values: {
     absenceReasonIds: string[];
   }) => {
+    const existingIds = filteredAbsenceReasonIds;
     const removedFromAbsReasons =
-      filteredAbsenceReasons?.filter(
-        ar => !values.absenceReasonIds.includes(ar.id)
-      ) ?? [];
-    const absReasons = absenceReasons.filter(ar =>
-      values.absenceReasonIds.includes(ar.id)
-    );
+      existingIds?.filter(ar => !values.absenceReasonIds.includes(ar)) ?? [];
+    const addedAbsReasons =
+      values.absenceReasonIds.filter(ar => !existingIds?.includes(ar)) ?? [];
 
     //handle removals
     for (let i = 0; i < removedFromAbsReasons.length ?? 0; i++) {
-      removedFromAbsReasons[i].positionTypeIds = removedFromAbsReasons[
-        i
-      ].positionTypeIds.filter((a: any) => props.positionTypeId);
-      await updateAbsenceReason({
-        variables: {
-          absenceReason: {
-            id: absReasons[i].id,
-            rowVersion: absReasons[i].rowVersion,
-            allowNegativeBalance: absReasons[i].allowNegativeBalance,
-            isRestricted: absReasons[i].isRestricted,
-            positionTypeIds: absReasons[i].positionTypeIds,
-          },
-        },
-      });
-    }
-
-    //handle additions
-    for (let i = 0; i < absReasons.length; i++) {
-      if (!absReasons[i]?.positionTypeIds.includes(props.positionTypeId)) {
-        absReasons[i].positionTypeIds.push(props.positionTypeId);
+      const ar = absenceReasons.find(a => a.id === removedFromAbsReasons[i]);
+      if (ar) {
+        const newPositionTypeIds = ar.positionTypeIds.filter(
+          (a: any) => props.positionTypeId !== a
+        );
 
         await updateAbsenceReason({
           variables: {
             absenceReason: {
-              id: absReasons[i].id,
-              rowVersion: absReasons[i].rowVersion,
-              allowNegativeBalance: absReasons[i].allowNegativeBalance,
-              isRestricted: absReasons[i].isRestricted,
-              positionTypeIds: absReasons[i].positionTypeIds,
+              id: ar.id,
+              rowVersion: ar.rowVersion,
+              allowNegativeBalance: ar.allowNegativeBalance,
+              isRestricted: ar.isRestricted,
+              positionTypeIds: newPositionTypeIds,
             },
           },
         });
       }
     }
-    setFilteredAbsenceReasons(absReasons);
+    //handle additions
+    for (let i = 0; i < addedAbsReasons.length; i++) {
+      const ar = absenceReasons.find(a => a.id === addedAbsReasons[i]);
+      if (ar && !ar.positionTypeIds.includes(props.positionTypeId)) {
+        ar.positionTypeIds.push(props.positionTypeId);
+        await updateAbsenceReason({
+          variables: {
+            absenceReason: {
+              id: ar.id,
+              rowVersion: ar.rowVersion,
+              allowNegativeBalance: ar.allowNegativeBalance,
+              isRestricted: ar.isRestricted,
+              positionTypeIds: ar.positionTypeIds,
+            },
+          },
+        });
+      }
+    }
+    setFilteredAbsenceReasonIds(values.absenceReasonIds);
   };
 
   if (absenceReasons.length === 0) {
     return <></>;
   }
 
-  if (!filteredAbsenceReasons) {
-    setFilteredAbsenceReasons(
-      absenceReasons.filter(ar =>
-        ar.positionTypeIds.includes(props.positionTypeId)
-      )
+  if (!filteredAbsenceReasonIds) {
+    setFilteredAbsenceReasonIds(
+      absenceReasons
+        .filter(ar => ar.positionTypeIds.includes(props.positionTypeId))
+        .map(ar => ar.id)
     );
   }
 
@@ -119,7 +129,7 @@ export const PositionTypeAbsReasonsCard: React.FC<Props> = props => {
       <Section>
         <Formik
           initialValues={{
-            absenceReasonIds: filteredAbsenceReasons?.map(pt => pt.id) ?? [],
+            absenceReasonIds: filteredAbsenceReasonIds ?? [],
           }}
           onSubmit={async (data, e) => {
             await handleUpdateAbsenceReason(data);
@@ -157,27 +167,68 @@ export const PositionTypeAbsReasonsCard: React.FC<Props> = props => {
               />
 
               {!editing &&
-                filteredAbsenceReasons &&
-                filteredAbsenceReasons.length === 0 && (
-                  <Typography>
-                    {t("Not associated with any absence reasons")}
-                  </Typography>
+                filteredAbsenceReasonIds &&
+                filteredAbsenceReasonIds.length === 0 && (
+                  <>
+                    <Typography>
+                      {t("Not associated with any absence reasons")}
+                    </Typography>
+                    <Typography>
+                      {t(
+                        "The following Absence Reasons are assoicated with this position type because they are assoicated with all position types:"
+                      )}
+                    </Typography>
+                  </>
                 )}
 
               {!editing &&
-                filteredAbsenceReasons &&
-                filteredAbsenceReasons.length > 0 && (
+                filteredAbsenceReasonIds &&
+                filteredAbsenceReasonIds.length > 0 && (
                   <>
-                    {filteredAbsenceReasons.map((ar, i) => (
+                    {filteredAbsenceReasonIds.map((ar, i) => (
+                      <Chip
+                        key={i}
+                        label={absenceReasons.find(a => a.id == ar)?.name}
+                        className={classes.positionTypeChip}
+                      />
+                    ))}
+                    {allPositionTypeAbsenceReasons.length > 0 && (
+                      <>
+                        <Typography className={classes.allReasonsText}>
+                          {t(
+                            "The following Absence Reasons are assoicated with this position type because they are assoicated with all position types:"
+                          )}
+                        </Typography>
+                        {allPositionTypeAbsenceReasons.map((ar, i) => (
+                          <Chip
+                            key={i}
+                            label={ar.name}
+                            className={classes.positionTypeChip}
+                          />
+                        ))}
+                      </>
+                    )}
+                  </>
+                )}
+              {editing && allPositionTypeAbsenceReasons.length > 0 && (
+                <>
+                  <Typography className={classes.allReasonsText}>
+                    {t(
+                      "The following Absence Reasons are assoicated with this position type because they are assoicated with all position types and can not be chosen:"
+                    )}
+                  </Typography>
+                  <div className={classes.chipContainer}>
+                    {allPositionTypeAbsenceReasons.map((ar, i) => (
                       <Chip
                         key={i}
                         label={ar.name}
                         className={classes.positionTypeChip}
                       />
                     ))}
-                  </>
-                )}
-
+                  </div>
+                  <Divider className={classes.divider} />
+                </>
+              )}
               {editing && (
                 <SelectNew
                   value={
@@ -209,5 +260,17 @@ const useStyles = makeStyles(theme => ({
     background: theme.customColors.blue,
     color: theme.customColors.white,
     marginRight: theme.spacing(1),
+  },
+  allReasonsText: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  chipContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  divider: {
+    color: theme.customColors.gray,
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(2),
   },
 }));
