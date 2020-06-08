e6e17a98d5d02759b34b0ae0b36552209d3447df
Corin (CWStra) <cwstra@gmail.com>
2020-06-08 11:08:57 -0400

    Remove console

--- modules/ui/pages/position-type/components/position-type-abs-reasons-card.tsx
+++ modules/ui/pages/position-type/components/position-type-abs-reasons-card.tsx
@@ -41,8 +41,6 @@ export const PositionTypeAbsReasonsCard: React.FC<Props> = props => {
     return <></>;
   }
 
-  console.log(sortedReasons);
-
   return (
     <Section>
       <SectionHeader title={t("Associated absence reasons")} />
