885ed56facefbb00290e7e40da542c3b466462f8
David Nawn <dnawn@edustaff.org>
2020-06-16 09:31:24 -0400

    Add local storage support to the 2 new canned reports

--- modules/ui/pages/reports/employee-balances/index.tsx
+++ modules/ui/pages/reports/employee-balances/index.tsx
@@ -3,11 +3,30 @@ import { Report } from "ui/components/reporting";
 import { useTranslation } from "react-i18next";
 import { useOrganizationId } from "core/org-context";
 import { useCurrentSchoolYear } from "reference-data/current-school-year";
+import {
+  getRdlFromLocalStorage,
+  saveRdlToLocalStorage,
+} from "ui/components/reporting/helpers";
 
 export const EmployeeBalancesReport: React.FC<{}> = () => {
   const { t } = useTranslation();
   const organizationId = useOrganizationId();
   const currentSchoolYear = useCurrentSchoolYear(organizationId ?? undefined);
+
+  // TODO: Once we have Saved Views, the need for this localStorage piece
+  // goes away. The localStorageKey has the Date in it on the off chance
+  // we need to come into here and modify the canned report RDL prior to
+  // implementing Saved Views and want to make sure all Users default back
+  // to the RDL that we define the next time they visit this report.
+  const localStorageKey = "AbsenceReasonBalancesReport_20200616";
+  const rdl = React.useMemo(() => {
+    const localStorageRdl = getRdlFromLocalStorage(localStorageKey);
+    if (localStorageRdl) {
+      return localStorageRdl;
+    }
+
+    return `QUERY FROM EmployeeBalances WHERE (SchoolYearId = ${currentSchoolYear?.id}) SELECT BalanceName, NetBalance, AbsenceReasonUsageUnit, SchoolYearName, AbsenceReasonName, AbsenceReasonCategoryName, InitialBalance, UsedBalance, UnusedBalance, Identifier AS 'Employee Identifier', Title, PositionTypeName, LocationNames ORDER BY Concat(LastName, ', ', FirstName) ASC WITH SUBTOTALS OrgUserId SHOW Concat(LastName, ', ', FirstName) AS Employee`;
+  }, [currentSchoolYear?.id]);
 
   if (!currentSchoolYear) {
     return <></>;
@@ -16,7 +35,7 @@ export const EmployeeBalancesReport: React.FC<{}> = () => {
   return (
     <Report
       title={t("Absence Reason Balances")}
-      rdl={`QUERY FROM EmployeeBalances WHERE (SchoolYearId = ${currentSchoolYear.id}) SELECT BalanceName, NetBalance, AbsenceReasonUsageUnit, SchoolYearName, AbsenceReasonName, AbsenceReasonCategoryName, InitialBalance, UsedBalance, UnusedBalance, Identifier AS 'Employee Identifier', Title, PositionTypeName, LocationNames ORDER BY Concat(LastName, ', ', FirstName) ASC WITH SUBTOTALS OrgUserId SHOW Concat(LastName, ', ', FirstName) AS Employee`}
+      rdl={rdl}
       exportFilename={t("AbsenceReasonBalancesReport")}
       allowedFilterFieldsOverride={[
         "SchoolYearId",
@@ -35,6 +54,7 @@ export const EmployeeBalancesReport: React.FC<{}> = () => {
       ]}
       showGroupLabels={false}
       sumRowData={false}
+      saveRdl={(rdl: string) => saveRdlToLocalStorage(localStorageKey, rdl)}
     />
   );
 };
