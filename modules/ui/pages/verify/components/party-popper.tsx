5c5752c0046e497c72779d8296785d06fb3005e5
Corin (CWStra) <cwstra@gmail.com>
2020-06-25 12:29:03 -0400

    Update react-confetti branch

--- modules/ui/pages/verify/components/party-popper.tsx
+++ modules/ui/pages/verify/components/party-popper.tsx
@@ -14,18 +14,31 @@ type Props = {};
 export const PartyPopper: React.FC<Props> = ({ children }) => {
   const classes = useStyles();
   const [ref, { x, y, width: w, height: h }] = useDimensions();
+  // We want to generate points along a line,
+  // so we make a function to do so.
+  // This will depend on the position and
+  // dimensions of the popper svg.
+  const getLinePoint = React.useCallback((): { x: number; y: number } => {
+    if (!x || !y || !w || !h) return { x: 0, y: 0 };
+    // The endpoints of the line, for our particular svg, are:
+    const p1 = [x + 0.23628 * w, y - 0.48127 * h];
+    const p2 = [x + 0.6521 * w, y - 0.11331 * h];
+    // Next, get a random number between 0 and 1
+    const r = Math.random();
+    // The closer r is to 0, the closer our generated point will be to p1,
+    // and likewise, the closer r is to 1, the closer to p2.
+    return {
+      x: p1[0] * (1 - r) + p2[0] * r,
+      y: p1[1] * (1 - r) + p2[1] * r,
+    };
+  }, [x, y, w, h]);
 
   return (
     <Grid container direction="column" alignItems="center">
       <Grid item className={classes.popper}>
         {x && y && w && h && (
           <RedRoverConfetti
-            confettiSource={{
-              x: x + 0.23628 * w ?? 0,
-              y: y - (1 - 0.31873) * h ?? 0,
-              side1: { x: 0, y: 0 },
-              side2: { x: (0.6521 - 0.23628) * w, y: (0.75669 - 0.31873) * h },
-            }}
+            confettiSource={{ getPoint: getLinePoint }}
             initialVelocityX={{ min: 1, max: 10 }}
             style={{ zIndex: 202 }}
           />
