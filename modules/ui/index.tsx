d4f9cb3d42bb9a783a54b36c82cd4ee4a90d6f1f
Frank Dzedzy <fdzedzy@redroverk12.com>
2020-07-22 11:56:12 -0400

    Missed a spot

--- modules/ui/index.tsx
+++ modules/ui/index.tsx
@@ -988,6 +988,7 @@ export const App = hot(function() {
                                         PermissionEnum.ScheduleSettingsView,
                                         PermissionEnum.AbsVacSettingsView,
                                         PermissionEnum.FinanceSettingsView,
+                                        PermissionEnum.ApprovalSettingsView,
                                       ]}
                                     />
                                     <ProtectedRoute
