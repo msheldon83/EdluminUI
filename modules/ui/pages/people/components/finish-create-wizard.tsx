3884890e69822f76cac092906995451c48bb8c19
Allen Dzedzy <adzedzy@edustaff.org>
2020-08-04 17:39:14 -0400

    Section added

--- modules/ui/pages/people/components/finish-create-wizard.tsx
+++ modules/ui/pages/people/components/finish-create-wizard.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { useIsMobile } from "hooks";
 import { makeStyles } from "@material-ui/styles";
+import { Section } from "ui/components/section";
+import { SectionHeader } from "ui/components/section-header";
 import { useTranslation } from "react-i18next";
 import { Formik } from "formik";
 import * as yup from "yup";
@@ -25,78 +27,81 @@ export const FinishWizard: React.FC<Props> = props => {
   const isMobile = useIsMobile();
 
   return (
-    <Formik
-      initialValues={{ inviteImmediately: true, createAnother: true }}
-      onSubmit={props.onSubmit}
-      validationSchema={yup.object().shape({
-        inviteImmediately: yup
-          .boolean()
-          .required(t("Invitation selection required")),
-        isRestricted: yup.boolean(),
-      })}
-    >
-      {({ values, handleSubmit, setFieldValue }) => (
-        <form onSubmit={handleSubmit}>
-          <Grid item xs={12} sm={6} lg={6}>
-            <Typography variant="h6" className={classes.label}>
-              {t(
-                `Do you want to invite ${props.orgUserName} to create their account right away?`
-              )}
-            </Typography>
-            <RadioGroup
-              aria-label="inviteImmediately"
-              name="inviteImmediately"
-              value={values.inviteImmediately}
-              onChange={e => {
-                setFieldValue("inviteImmediately", e.target.value === "true");
-              }}
-              row={!isMobile}
-            >
-              <FormControlLabel
-                value={false}
-                control={<Radio color="primary" />}
-                label={t("No")}
-                labelPlacement="end"
-              />
-              <FormControlLabel
-                value={true}
-                control={<Radio color="primary" />}
-                label={t("Yes")}
-                labelPlacement="end"
-              />
-            </RadioGroup>
-          </Grid>
+    <Section>
+      <SectionHeader title={t("Basic info")} />
+      <Formik
+        initialValues={{ inviteImmediately: true, createAnother: true }}
+        onSubmit={props.onSubmit}
+        validationSchema={yup.object().shape({
+          inviteImmediately: yup
+            .boolean()
+            .required(t("Invitation selection required")),
+          isRestricted: yup.boolean(),
+        })}
+      >
+        {({ values, handleSubmit, setFieldValue }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid item xs={12} sm={6} lg={6}>
+              <Typography variant="h6" className={classes.label}>
+                {t(
+                  `Do you want to invite ${props.orgUserName} to create their account right away?`
+                )}
+              </Typography>
+              <RadioGroup
+                aria-label="inviteImmediately"
+                name="inviteImmediately"
+                value={values.inviteImmediately}
+                onChange={e => {
+                  setFieldValue("inviteImmediately", e.target.value === "true");
+                }}
+                row={!isMobile}
+              >
+                <FormControlLabel
+                  value={false}
+                  control={<Radio color="primary" />}
+                  label={t("No")}
+                  labelPlacement="end"
+                />
+                <FormControlLabel
+                  value={true}
+                  control={<Radio color="primary" />}
+                  label={t("Yes")}
+                  labelPlacement="end"
+                />
+              </RadioGroup>
+            </Grid>
 
-          <Grid item xs={12} sm={6} lg={6}>
-            <Typography variant="h6" className={classes.label}>
-              {t(`Do you want to create another ${props.orgUserType}?`)}
-            </Typography>
-            <RadioGroup
-              aria-label="createAnother"
-              name="createAnother"
-              value={values.createAnother}
-              onChange={e => {
-                setFieldValue("createAnother", e.target.value === "true");
-              }}
-              row={!isMobile}
-            >
-              <FormControlLabel
-                value={false}
-                control={<Radio color="primary" />}
-                label={t("No")}
-                labelPlacement="end"
-              />
-              <FormControlLabel
-                value={true}
-                control={<Radio color="primary" />}
-                label={t("Yes")}
-                labelPlacement="end"
-              />
-            </RadioGroup>
-          </Grid>
-        </form>
-      )}
-    </Formik>
+            <Grid item xs={12} sm={6} lg={6}>
+              <Typography variant="h6" className={classes.label}>
+                {t(`Do you want to create another ${props.orgUserType}?`)}
+              </Typography>
+              <RadioGroup
+                aria-label="createAnother"
+                name="createAnother"
+                value={values.createAnother}
+                onChange={e => {
+                  setFieldValue("createAnother", e.target.value === "true");
+                }}
+                row={!isMobile}
+              >
+                <FormControlLabel
+                  value={false}
+                  control={<Radio color="primary" />}
+                  label={t("No")}
+                  labelPlacement="end"
+                />
+                <FormControlLabel
+                  value={true}
+                  control={<Radio color="primary" />}
+                  label={t("Yes")}
+                  labelPlacement="end"
+                />
+              </RadioGroup>
+            </Grid>
+          </form>
+        )}
+      </Formik>
+    </Section>
   );
 };
 
